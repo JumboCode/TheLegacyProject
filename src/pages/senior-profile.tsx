c5255dc15357c0fb816e12835c3e5ae6e793aef7
Jo Chung <chjoanna@163.com>
2022-11-13 14:56:51 -0500

    Changed date format

--- src/pages/senior-profile.tsx
+++ src/pages/senior-profile.tsx
@@ -2,11 +2,13 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { format } from "date-fns";
 import { date } from "zod";
+//import pdf from './icons/icon_pdf.png';
 
 enum FileType {
-  Document,
-  Pdf,
-  Audio
+  Document = "https://freeiconshop.com/wp-content/uploads/edd/doc-outline.png",
+  Pdf = "https://parkcam.com.tr/wp-content/uploads/2021/12/pdf_icon.jpg",
+  //Pdf = "./icons/icon_pdf.png",
+  Audio = "https://ecdn.teacherspayteachers.com/thumbitem/Demo-recording-The-Orffin-Blues--8080847-1657611393/original-8080847-1.jpg"
 }
 
 type fileProps =  {
@@ -17,6 +19,7 @@ type fileProps =  {
 };
 
 const FileGrid: NextPage = () => {
+  
     return (
         <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
             <h1 className="text-xl font-extrabold leading-normal text-gray-700 md:text-[5rem]">
@@ -61,19 +64,25 @@ const File = ({
     type,
     last_modified,
 }: fileProps) => {
-    return (
-        <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{type}</p>
-      <p className="text-sm text-gray-600">{format(last_modified, "MMMM do, yyyy H:mma")}</p>
-      <a
-        className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
-        href={URL}
-      >
-        Link
-      </a>
-      if ({type} == {FileType.Pdf})<img src="/icons/icon_pdf.png"></img>
-      
-    </section>
-    );
+  const formattedDate: () => String = () => {
+    let dateStr = `Last modified on ${last_modified.toDateString()} at ${last_modified.toTimeString()}`;
+    return dateStr.substring(0, dateStr.indexOf("G"));
+  }
+
+
+  return (
+    <section className="flex flex-col justify-center rounded border-2 border-gray-500 p-6 shadow-xl duration-500 motion-safe:hover:scale-105">
+    <h2 className="text-lg text-gray-700">{name}</h2>
+    {/* <p className="text-sm text-gray-600">{type}</p> */}
+    <p className="text-sm text-gray-600">{formattedDate()}</p>
+    <a
+      className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
+      href={URL}
+    >
+      Link
+    </a>
+    <img className ="object-scale-down h-10" src={type}></img> 
+    
+  </section>
+  );
 };
