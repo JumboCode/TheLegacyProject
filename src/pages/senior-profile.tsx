aa9db11cc8cd7a75dd16cb2e3a0605d128a145d4
Jo Chung <chjoanna@163.com>
2022-11-20 19:40:39 -0500

    Added SearchBar and AddFile
    Co-authored-by: taarikabala <taarikabala@users.noreply.github.com>
    Co-authored-by: Joe Carpenter <JosephCarpenter@users.noreply.github.com>
    Co-authored-by: Amitav Nott <anott03@users.noreply.github.com>

--- src/pages/senior-profile.tsx
+++ src/pages/senior-profile.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image"
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
 
 enum FileType {
   Document = "/icons/icon_doc.png",
@@ -14,6 +14,43 @@ type fileProps =  {
     type: FileType
     last_modified: Date
 };
+
+const SearchBar = ({
+  data,
+  setData
+}:{
+  data: fileProps[],
+  setData: Dispatch<SetStateAction<fileProps[]>>
+}) => {
+
+  const [searchInput, setSearchInput] = useState("");
+  const originalData = useRef(data);
+  //search files by name only
+  //const [searchParam] = useState(["name"]);
+
+  const handleChange = (e: any) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+
+    if (searchInput.length === 0) {
+      setData(originalData.current);
+    } else if (searchInput.length > 0){
+      //function to filter files
+      const filteredData = originalData.current.filter(x => {
+        return x.name.includes(searchInput);
+      });
+      setData(filteredData);
+      //data.name.toLowerCase().includes(searchInput.toLowerCase());
+    }
+  };
+
+
+    
+  return (
+    <input className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50"
+           type="text" placeholder="Search" onChange={handleChange} value={searchInput} />
+  );
+}
 
 const AddFile = ({
     data,
@@ -36,24 +73,25 @@ const AddFile = ({
 const FileGrid: NextPage = () => {
 
   //function to create new files
-    const [ data, setData ] = useState<fileProps[]>([]);
-    useEffect(() => {
-      setData([
-        {name: "example_file5.mp3", type: FileType.Audio, URL: "", last_modified: new Date()},
-        {name: "example_file6.pdf", type: FileType.Pdf, URL: "", last_modified: new Date()},
-        {name: "example_file7.doc", type: FileType.Document, URL : "", last_modified: new Date()},
-        {name: "example_file8.mp3", type: FileType.Audio, URL : "", last_modified: new Date()},
-      ]);
-    }, []) 
+    const [ data, setData ] = useState<fileProps[]>([
+      {name: "example_file5.mp3", type: FileType.Audio, URL: "", last_modified: new Date()},
+      {name: "example_file6.pdf", type: FileType.Pdf, URL: "", last_modified: new Date()},
+      {name: "example_file7.doc", type: FileType.Document, URL : "", last_modified: new Date()},
+      {name: "example_file8.mp3", type: FileType.Audio, URL : "", last_modified: new Date()},
+    ]);
+    //useEffect(() => {
+   //   setData();
+   // }, []) 
 
     return (
         <main className="container flex min-h-screen flex-col p-4">
           <h1 className="text-[3rem] leading-normal text-gray-700">File Grid</h1>
+          <SearchBar data={data as fileProps[]} setData={setData}/>
           <div className="mt-3 grid gap-3 pt-3 text-center md:grid-cols-4">
             <AddFile data={data} setData={setData}/>
            
-           { data.map(({ name, URL, type, last_modified}: fileProps) =>
-              <File key={name} name={name} URL={URL} type={type} last_modified={last_modified}/>) }
+           {data.map(({ name, URL, type, last_modified}: fileProps) =>
+              <File key={name} name={name} URL={URL} type={type} last_modified={last_modified}/>)}
            
           </div>
         </main>
@@ -78,7 +116,7 @@ const File = ({
       <Image className="object-scale-down h-10" width={35} height={35} src={type} alt="file icon"></Image>
       <h2 className="text-lg text-gray-700">{name}</h2>
       {/* <p className="text-sm text-gray-600">{type}</p> */}
-      <p className="text-sm text-gray-600">{formattedDate()}</p>
+      {/* <p className="text-sm text-gray-600">{formattedDate()}</p> */}
       <a
         className="m-auto mt-3 w-fit text-sm text-violet-500 underline decoration-dotted underline-offset-2"
         href={URL}
