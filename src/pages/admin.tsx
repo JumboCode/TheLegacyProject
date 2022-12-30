c28019d92a5bd0dac89123ae9f85720ff626943a
Amitav Nott <58123848+anott03@users.noreply.github.com>
2022-12-29 21:51:19 -0500

    making the admin template a bit more responsive

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from "next";
-import Sidebar from "@components/sidebar";
 import { useState } from "react";
 
 function TabToggle({ children, active, onClick }: { children: any, active: boolean, onClick: any }) {
@@ -18,22 +17,22 @@ function TabToggle({ children, active, onClick }: { children: any, active: boole
 function Card({ type, data }: { type: string, data: any }) {
   if (type == "member") {
     return (
-      <div className="flex flex-col m-3 bg-white p-9 rounded-lg items-center justify-center border-2 border-white hover:border-green hover:cursor-pointer duration-300">
+      <div className="flex flex-col m-3 bg-white p-9 rounded-lg items-center justify-between border-2 border-white text-center hover:border-green hover:cursor-pointer duration-300">
         <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px] bg-slate-300">
           Profile Picture
         </div>
-        <p>{data.name}</p>
-        <p>{data.email}</p>
+        <p className="py-3">{data.name}</p>
+        <small className="font-light">{data.email}</small>
       </div>
     );
   }
   return (
-    <div className="flex flex-col m-3 bg-white p-9 rounded-lg items-center justify-center border-2 border-white hover:border-green hover:cursor-pointer">
+    <div className="flex flex-col m-3 bg-white p-9 rounded-lg items-center justify-between border-2 border-white text-center hover:border-green hover:cursor-pointer">
       <div className="min-w-[100px] max-w-[100px] min-h-[100px] max-h-[100px] bg-slate-300">
         Profile Picture
       </div>
-      <p>{data.name}</p>
-      <p>{data.location}</p>
+      <p className="py-3">{data.name}</p>
+      <small className="font-light">{data.location}</small>
     </div>
   );
 }
@@ -74,32 +73,34 @@ const Admin: NextPage = () => {
   return (
     // this outer div can be though of as the "layout" for the admin page, which
     // might be a concept worth abstracting out to it's own component later.
-    <div className="bg-off-white w-full h-full flex-[0.85]">
-      {/* the header */}
-      <div className="flex flex-row items-center justify-between pt-9 px-5">
-        <h1 className="text-2xl">General</h1>
-        <div className="min-w-[50px] min-h-[50px] bg-green text-white">Icon</div>
-      </div>
-      {/* the toggles between members and seniors */}
-      <div className="flex flex-row">
-        <TabToggle active={activeTab == 0} onClick={toggleActiveTab}>Members</TabToggle>
-        <TabToggle active={activeTab == 1} onClick={toggleActiveTab}>Seniors</TabToggle>
-      </div>
-      {/* the search box */}
-      <div>
-        <input type="text" className="ml-5 p-1 border border-1 border-green rounded-lg bg-off-white w-[75%]"/>
-      </div>
-      {/* the list of members/seniors */}
-      <div className="grid grid-cols-4 p-5 pl-0">
-        { /* <div className="flex flex-col m-3 bg-white p-9 rounded-lg justify-center items-center text-[2rem] hover:border-2 hover:border-green hover:cursor-pointer"> */ }
-        <div className="flex flex-col m-3 bg-white p-9 rounded-lg justify-center items-center text-[2rem] border-2 border-white hover:bg-green hover:text-white hover:cursor-pointer duration-300" onClick={addButtonClicked}>
-        +
+    <div className="flex-[0.85] flex flex-col items-center">
+      <div className="bg-off-white w-full h-full px-3">
+        {/* the header */}
+        <div className="flex flex-row items-center justify-between pt-9 px-5">
+          <h1 className="text-2xl">General</h1>
+          <div className="min-w-[50px] min-h-[50px] bg-green text-white">Icon</div>
         </div>
-        {
-          (activeTab == 0 ? dummyMemberData : dummySeniorData).map((data: any, i: number) => (
-            <Card key={i} type={ activeTab == 0 ? "member" : "senior" } data={data}/>
-          ))
-        }
+        {/* the toggles between members and seniors */}
+        <div className="flex flex-row">
+          <TabToggle active={activeTab == 0} onClick={toggleActiveTab}>Members</TabToggle>
+          <TabToggle active={activeTab == 1} onClick={toggleActiveTab}>Seniors</TabToggle>
+        </div>
+        {/* the search box */}
+        <div>
+          <input type="text" className="ml-5 p-1 border border-1 border-green rounded-lg bg-off-white w-[75%]"/>
+        </div>
+        {/* the list of members/seniors */}
+        <div className="grid sm: grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 p-5 pl-0">
+          { /* <div className="flex flex-col m-3 bg-white p-9 rounded-lg justify-center items-center text-[2rem] hover:border-2 hover:border-green hover:cursor-pointer"> */ }
+          <div className="flex flex-col m-3 bg-white p-9 rounded-lg justify-center items-center text-[2rem] border-2 border-white hover:bg-green hover:text-white hover:cursor-pointer duration-300" onClick={addButtonClicked}>
+          +
+          </div>
+          {
+            (activeTab == 0 ? dummyMemberData : dummySeniorData).map((data: any, i: number) => (
+              <Card key={i} type={ activeTab == 0 ? "member" : "senior" } data={data}/>
+            ))
+          }
+        </div>
       </div>
     </div>
   );
