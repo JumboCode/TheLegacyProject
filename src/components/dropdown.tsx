7e4004d15ce04d89484c08b7519f1ab0b8c334b9
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-02-02 17:07:21 -0500

    Dropdown component (#37)
    * a working dropown component with a funky paradigm
    
    * wip
    
    * PR Prep
    + reverting index.tsx now that testing is done
    + removing the useDropdown hook and making Dropdown the default export
      per conversation with Michael.

---
+++ src/components/dropdown.tsx
@@ -0,0 +1,49 @@
+import { Dispatch, MouseEventHandler, SetStateAction, useEffect, useState } from "react";
+
+type DropdownProps = {
+    items: string[],
+    bgColor: string
+    selected: string,
+    setSelected: Dispatch<SetStateAction<string>>
+}
+
+type ItemProps = {
+    text: string,
+    onClick: MouseEventHandler<HTMLDivElement> | undefined,
+    selected?: boolean,
+    bgColor: string
+}
+
+function Item({ text, onClick, selected, bgColor }: ItemProps) {
+    let [styles, setStyles] = useState<string>("");
+
+    useEffect(() => {
+        setStyles(`w-full px-5 py-3 hover:${bgColor} hover:text-white cursor-pointer ${selected ? `bg-${bgColor}-600 text-white`: ''}`);
+    });
+
+    return (
+        <div className={styles} onClick={onClick}>
+            <p>{text}</p>
+        </div>
+    );
+}
+
+export default function Dropdown({ items, bgColor, selected, setSelected }: DropdownProps): JSX.Element {
+    let [open, setOpen] = useState<boolean>(false);
+
+    const styles = () => {
+        // return `md:min-w-[10rem] relative ${open ? "shadow box-shadow drop-shadow" : ""}`
+        return "md:min-w-[10rem] relative shadow-lg box-shadow-lg drop-shadow-lg"
+    }
+
+    return (
+        <div className={styles()}>
+            <div className="bg-white absolute top-0 left-0 min-w-[10rem]" onClick={() => setOpen(!open)}>
+                <Item text={selected} onClick={() => setOpen(!open)} bgColor={bgColor} selected/>
+                {open &&items.map((item: string, i: number) =>
+                    <Item key={i} text={item} onClick={() => setSelected(items[i] || "")} bgColor={bgColor}/>
+                )}
+            </div>
+        </div>
+    );
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,11 @@
 const colors = require('tailwindcss/colors');
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  // safelist: [
+  //   {
+  //     pattern: /-*/,
+  //   }
+  // ],
   theme: {
     extend: {},
     colors: {
