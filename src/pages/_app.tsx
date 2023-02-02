62514828fb40b2fe9dd218e8baf249a1134085df
sgilfeather <skylar.gilfeather@gmail.com>
2023-02-01 23:07:09 -0500

    smoother FileGrid

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,18 +2,35 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-import { Navbar } from "@components/navbar";
+import Sidebar from "@components/sidebar";
+import Navbar from "@components/navbar/Navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-  return (
-    <SessionProvider session={session}>
-      <Navbar />
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
+
+  if (/* is it the Landing Page? */) {
+    return (
+        <SessionProvider session={session}>
+            <div className="flex flex-col w-screen h-screen">
+                <Navbar />
+                <Component {...pageProps} />
+            </div>
+        </SessionProvider>
+    );
+  }
+  else {
+    return (
+        <SessionProvider session={session}>
+            <div className="flex flex-row w-screen h-screen">
+                <Sidebar />
+                <Component {...pageProps} />
+            </div>
+        </SessionProvider>
+    );
+  }
+
 };
 
 export default MyApp;
