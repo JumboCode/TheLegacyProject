fe058ca08fdf7f3c1a77cc688abc0443b2f5e130
taarikabala1 <taarikabala@gmail.com>
2023-02-03 16:37:58 -0500

    Added galleryDemo page and began creating photoGallery component

---
+++ src/components/photoGallery/photoCard.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+
+export const PhotoCard = () => {
+    return(
+        <div className="">
+        </div>
+    );
+}
---
+++ src/components/photoGallery/photoGallery.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+// import PhotoCard
+
+const PhotoGallery = () => {
+  const [currImageIndex, setCurrImageIndex] = useState(0);
+
+  // const prevImage = () => {
+  //   currImageIndex--
+  // };
+
+  // const nextImage = () => {
+  //   currImage1Index++
+  //   currImage2Index++
+  //    .map(Image, index) -> index the image for us 
+  // };
+
+  return (
+    <>
+      <div className='max-w-[1440px] max-h-[684px] w-full m-auto relative bg-[#F5F0EA]'>
+        <div className=''>
+          
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default PhotoGallery;
