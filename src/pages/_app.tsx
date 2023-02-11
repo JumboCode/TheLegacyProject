3a9f0e6da6a509a54c4acb99e5d5061c1a57a48e
siarasmall <41011312+siarasmall@users.noreply.github.com>
2023-02-11 11:49:40 -0500

    trying to merge with michael's auth branch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,34 +2,18 @@ import "../styles/globals.css";
 import type { AppType } from "next/app";
 import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
-// TODO: merge sidebar code to incorporate sidebar for _app
 import { Navbar } from "@components/navbar";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
   pageProps: { session, ...pageProps },
 }) => {
-
-  if (true) {
-    return (
-        <SessionProvider session={session}>
-            <div className="flex flex-col w-screen h-screen">
-                <Navbar />
-                <Component {...pageProps} />
-            </div>
-        </SessionProvider>
-    );
-  }
-  else {
-    return (
-        <SessionProvider session={session}>
-            <div className="flex flex-row w-screen h-screen">
-                <Sidebar />
-                <Component {...pageProps} />
-            </div>
-        </SessionProvider>
-    );
-  }
+  return (
+    <SessionProvider session={session}>
+      <Navbar />
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default MyApp;
