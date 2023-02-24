1aa02fe38978307d5c6083becb93bca147645370
Bowie Bello <obello26@icloud.com>
2023-02-24 17:44:30 -0500

    Added circular image next to email sign up box Deleted inline-block class from emailSignupBox because flex was already there

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { EmailSignupBox } from "@components/emailSignup";
+import Image from "next/image";
 
 const Home: NextPage = () => {
   return (
@@ -10,19 +11,19 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-        <main className="container mx-auto flex min-h-screen bg-off-white flex-col items-center p-4">
-          <div> 
-            <div> {/*left*/}
-                <h1 className="text-5xl font-extrabold leading-normal text-dark-plum md:text-[5rem] my-4 border-2 border-red-500">
+        <main className="container mx-auto flex min-h-screen bg-off-white flex-col items-center justify-items-center p-4">
+          <div className="flex flex-row gap-5 items-center justify-items-center border-2 border-green-500"> {/*holds left and right*/}
+            <div className="w-1/2 border-2 border-blue-500"> {/*left: email signup*/}
+                <h1 className="text-5xl font-extrabold leading-tight text-dark-plum md:text-[4rem] my-4 border-2 border-red-500">
                 Everyone has a story worth preserving
                 </h1>
                 <p className="text-2xl text-gray-700 bg-off-white my-4">Legacy Project documents the stories of the elder generation
 to ensure that their legacies are preserved for years to come.</p>
                 <EmailSignupBox />
             </div>
-           <div> {/*right*/}
-
-           </div>
+            <div className="w-1/2 flex flex-col items-center justify-items-center border-2 border-blue-500"> {/*right: photo*/}
+                <Image src="/../public/home/homepage-circle.png" alt="A circular photo of younger woman helping a senior citizen" height="500px" width="500px"></Image>
+            </div>
             
         </div>
       
