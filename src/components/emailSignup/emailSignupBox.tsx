562bf7f0a5316a9c50317be9bfbd4e6c1e6ca86f
Bowie Bello <obello26@icloud.com>
2023-03-05 22:35:17 -0500

    Deleted heroLanding.tsx Removed borders around components that were just for aiding styling in index.tsx and emailSignupBox.tsx Changed height and width of remaining flower/paper images  to use {} notation Added alts to remaining images

--- src/components/emailSignup/emailSignupBox.tsx
+++ src/components/emailSignup/emailSignupBox.tsx
@@ -8,7 +8,7 @@ function handleSubmit(event) {
 
 const EmailSignupBox = () => {
     return (
-        <form onSubmit={handleSubmit} className="flex flew-row gap-3 content-center p-4 border-2 border-red-500 bg-off-white">
+        <form onSubmit={handleSubmit} className="flex flew-row gap-3 content-center p-4 bg-off-white">
             <input type="email" id="email" className="bg-off-white border border-gray-300 text-dark-gray text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 w-3/4" placeholder="john.doe@company.com" required />
             <input type="submit" value="Join E-List" className="text-l rounded-lg bg-dark-green py-1.5 px-4 font-sans text-white duration-150 hover:-translate-y-0.5 w-1/4"></input>
         </form>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,27 +12,27 @@ const Home: NextPage = () => {
       </Head>
       
         <main className="container mx-auto flex min-h-screen bg-off-white flex-col items-center justify-items-center p-4">
-          <div className="flex flex-row gap-5 items-center justify-items-center border-2 border-green-500"> {/*holds left and right*/}
-            <div className=" border-2 border-blue-500"> {/*left: email signup*/}
-                <h1 className="text-5xl font-extrabold leading-tight text-dark-plum md:text-[4rem] my-4 border-2 border-red-500 duration-500">
+          <div className="flex flex-row gap-5 items-center justify-items-center"> {/*holds left and right*/}
+            <div className=""> {/*left: email signup*/}
+                <h1 className="text-5xl font-extrabold leading-tight text-dark-plum md:text-[4rem] my-4 duration-500">
                 Everyone has a story worth preserving
                 </h1>
                 <p className="text-2xl text-gray-700 bg-off-white my-4 duration-500">Legacy Project documents the stories of the elder generation
 to ensure that their legacies are preserved for years to come.</p>
                 <EmailSignupBox />
             </div>
-            <div className="w-[600px] hidden lg:flex shrink-0 items-center justify-center border-2 border-blue-500 h-[600px] relative"> {/*right: photo*/}
-                <div className="h-[500px] border-2 border-green-200">
+            <div className="w-[600px] hidden lg:flex shrink-0 items-center justify-center h-[600px] relative"> {/*right: photo*/}
+                <div className="h-[500px]">
                   <Image src="/home/homepage-circle.png" alt="A circular photo of younger woman helping a senior citizen" height={500} width={500} className="absolute z-10 duration-500"></Image>
                 </div>
                 <div className="absolute top-0 -left-24">
-                  <Image src="/home/pinkflower.png" height={323} width={323} className="absolute z-0 duration-500"></Image>
+                  <Image src="/home/pinkflower.png" height={323} width={323} alt="Image of a branch with small dried pink flowers with a transparent background" className="absolute z-0 duration-500"></Image>
                 </div>
                 <div className="absolute -bottom-10 -right-6 z-20">
-                  <Image src="/../public/home/yellowflower.png" height="246" width="246" className="duration-500"></Image>
+                  <Image src="/../public/home/yellowflower.png" height={246} width={246} alt="Image of a single yellow-orange dried flower with a transparent background" className="duration-500"></Image>
                 </div>
                 <div className="absolute -top-20 -right-8">  
-                  <Image src="/../public/home/paper.png" height="368" width="274" className="duration-500"></Image>
+                  <Image src="/../public/home/paper.png" height={368} width={274} alt="Image of an aged piece of paper with cursive writing on it" className="duration-500"></Image>
                 </div>
             </div>
             
