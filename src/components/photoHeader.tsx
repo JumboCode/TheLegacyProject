e3a80ba878aaa87a994f9704dc4b09492da8f900
taarikabala1 <taarikabala@gmail.com>
2023-03-10 16:32:39 -0500

    Started transparent overlay box under header image and fixed profile tile

--- src/components/photoHeader.tsx
+++ src/components/photoHeader.tsx
@@ -5,16 +5,19 @@ import Image from "next/image"
 
 const PhotoHeader = () => {
     return (
-        <div className='lg:p-9 md:p-5 p-3 w-full'> 
+        <>
+        <div className='lg:p-9 md:p-5 p-3'> 
             <Image className='rounded-lg object-cover'
             src={'/student_home/header1.jpg'} 
             alt={'Photo of flowers on an old book with a faded photo of two people'}
-            width={1500} 
+            width={2000} 
             height={500}
             />
         </div>
-        
-
+        <div className='lg:p-0 md:p-5 p-3 border-2 border-blue-500 bg-white-500/75'>
+            
+        </div> 
+        </>
     );
 }
 
--- src/components/profileTile/index.tsx
+++ src/components/profileTile/index.tsx
@@ -9,11 +9,11 @@ type tileData =  {
 const ProfileTile = ({ name, location, picture }: tileData) => {
         return (
         <div className="lg:p-9 md:p-5 p-3">
-          <section className="w-64 h-64 place-content-center rounded border bg-white drop-shadow-md">
-            <div className="align-center">
-                <Image className="object-scale-down h-10 justify-center" width={35} height={35} src={'/student_home/profile_tile_pic1.png'} alt="Placeholder profile image"></Image>
-                <p className="text-lg text-gray-700 justify-center">{name}</p>
-                <p className="text-sm text-gray-600 justify-center">Test</p>
+          <section className="w-64 h-64 p-4 place-content-center rounded border bg-white drop-shadow-md">
+            <div className="align-center flex flex-col width-full">
+                <Image className="object-scale-down h-10" width={35} height={35} src={'/student_home/profile_tile_pic1.png'} alt="Placeholder profile image"></Image>
+                <p className="text-lg text-gray-700">{name}</p>
+                <p className="text-sm text-gray-600">Test</p>
             </div>
           </section>
         </div>
