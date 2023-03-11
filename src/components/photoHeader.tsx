3f840664540ffd875102210d46ab10f5a5537d75
Alyssa Williams <alyssa.williams.tk360@gmail.com>
2023-03-10 20:07:35 -0500

    fixing font sizes

--- src/components/photoHeader.tsx
+++ src/components/photoHeader.tsx
@@ -17,8 +17,8 @@ const PhotoHeader = () => {
                     <div className='rounded-lg bg-white bg-opacity-90 backdrop-blur-sm w-[98%] h-20 absolute right-0 -bottom-12 md:-bottom-10 flex p-3 items-center'>
                         <Image className='rounded-lg' src={'/student_home/profile_photo_pic.jpeg'} alt={'Profile Photo'} width={56} height={56}/>
                         <div className='flex flex-col pl-3 text-neutral-600'>
-                            <h5 className='font-bold'>Angela Han</h5>
-                            <p className=''>email@legacy.org</p>
+                            <h5 className='font-bold text-base'>Angela Han</h5>
+                            <p className='text-sm'>email@legacy.org</p>
                         </div>
                         
                     </div>
