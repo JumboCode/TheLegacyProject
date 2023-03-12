36a546b66fbea97b83a1963073b6d62faa76b8dc
siarasmall <41011312+siarasmall@users.noreply.github.com>
2023-03-12 19:38:04 -0400

    finished student api routes (#55)
    * finished student api routes
    
    * Delete add.tsx
    
    This file was empty
    
    * rename dir
    
    ---------
    
    Co-authored-by: Michael Simpson <michaelsimpson2019@gmail.com>
    Co-authored-by: Michael Simpson <michael.simpson@tufts.edu>

---
+++ src/pages/api/student/add.ts
@@ -0,0 +1,49 @@
+import { getServerAuthSession } from "@server/common/get-server-auth-session";
+import { prisma } from "@server/db/client";
+import { Session } from "inspector";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const student = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerAuthSession({req, res});
+    const current_user = await prisma.user.findUnique({
+        where: {
+            id: session?.user?.id
+        }
+    })
+    if (current_user?.admin) {
+        if (req.method == 'POST') {
+            try {
+                const body = JSON.parse(req.body);
+                if (body) {
+                    const student = await prisma.user.create({ 
+                        data: { 
+                            id: body.id,
+                            name: body.name,
+                            email: body.email,
+                            emailVerified: body.emailVerified,
+                            image: body.image,
+                            admin: body.admin,
+                            Account: body.Account,
+                            Session: body.Session,
+                            SeniorIDs: body.SeniorIDS,
+                            Seniors: body.Seniors,
+
+                        }
+                    })
+                
+                } else {
+                    res.status(500).json({
+                        error: `Student creation request not well defined`,
+                        });
+                }
+            } catch { 
+                res.status(500).json({
+                error: `Failed to create student`,
+                });
+            
+            }
+        }
+    }  
+}
+
+export default student;
---
+++ src/pages/api/students/index.tsx
@@ -0,0 +1,27 @@
+import { getServerAuthSession } from "@server/common/get-server-auth-session";
+import { prisma } from "@server/db/client";
+import { Session } from "inspector";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const student = async (req: NextApiRequest, res: NextApiResponse) => {
+    const session = await getServerAuthSession({req, res});
+    const current_user = await prisma.user.findUnique({
+        where: {
+            id: session?.user?.id
+        }
+    })
+    if(req.method == 'GET') {
+        if (current_user?.admin) {
+            //returns list of students
+            try {
+                const student = await prisma.user.findMany();
+            } catch {
+                res.status(500).json({
+                    error: `Failed to access student database`,
+                    });
+            }
+        }
+    }
+}
+
+export default student;
