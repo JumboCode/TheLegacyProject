8eca145eeb881ccf2f59cb4bacabcc31d8aa044c
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-03-12 20:01:20 -0400

    added googleapis package

--- src/pages/api/drive/drive.ts
+++ src/pages/api/drive/drive.ts
@@ -1,11 +1,14 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from 'next-auth/jwt';
 
+import { google } from 'googleapis';
+
 const drive = async (req: NextApiRequest, res: NextApiResponse) => {
     const token = await getToken({ req });
 
     // here we do the things to get stuff from the drive api and return it to
     // the frontend
+    console.log(google.drive);
 
     res.status(200).json({
         message: "this is placeholder response",
