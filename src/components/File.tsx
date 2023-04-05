5f10fa59c3d4620cd8c618ba964ed1a726b586ca
sgilfeather <skylar.gilfeather@gmail.com>
2023-04-05 17:21:07 -0400

    removing Tags[], photo in Senior model, remove fileIcon in FileProps

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -3,21 +3,19 @@ import Image from "next/image";
 type FileProps =  {
     name: string,
     URL: string,
-    fileIcon: Image,
+    fileType: string,
     lastModified: Date
 };
 
 const File = ({
     name,
     URL,
-    fileIcon,
+    fileType,
     lastModified
 }: FileProps) => {
     return (
         <div className="flex flex-col text-left font-sans p-4 bg-off-white rounded-md aspect-square border hover:cursor-pointer hover:bg-taupe-hover">
-            <div className="flex flex-row justify-items-end w-full font-semibold"> {name} 
-                <Image className="" width={75} height={75} src={fileIcon} />
-            </div>
+            <span> {fileType} </span>
             <span> {URL} </span>
             <span> {lastModified} </span>
         </div>
