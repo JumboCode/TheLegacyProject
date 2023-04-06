bc6e69bc17649cc14a8016edf6baa2e519189aa1
Michael Simpson <michael.simpson@tufts.edu>
2023-04-06 18:28:00 -0400

    initial schema changes

---
+++ src/pages/api/admin/index.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getServerAuthSession } from "@server/common/get-server-auth-session";
+
+const settings = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerAuthSession({ req, res });
+
+  if (session) {
+    res.send({
+      content:
+        "This is protected content. You can access this content because you are signed in.",
+    });
+  } else {
+    res.send({
+      error:
+        "You must be signed in to view the protected content on this page.",
+    });
+  }
+};
+
+export default settings;
--- src/pages/api/button/[bid].ts
+++
@@ -1,3 +0,0 @@
-class Button {
-    const button = async (req: NextApiRequest, res: NextApiResponse) => {  }
-}
