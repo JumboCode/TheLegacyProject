671d71cbe6e4ac109dd7eba70a972ada7e6b38dd
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-04-07 16:32:19 -0400

    work in progress

--- src/components/AddFile.tsx
+++ src/components/AddFile.tsx
@@ -1,9 +1,31 @@
 import React, { useState } from "react";
 
+// https://feathericons.dev/?search=x&iconset=feather&format=strict-tsx
+function X(props: JSX.IntrinsicElements["svg"]) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      width="24"
+      height="24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      {...props}
+    >
+      <line x1="18" x2="6" y1="6" y2="18" />
+      <line x1="6" x2="18" y1="6" y2="18" />
+    </svg>
+  );
+}
+
 const Tag = ({ text }: { text: string }) => {
   return (
-    <div className="rounded-xl bg-gray-300 py-1 px-3">
+    <div className="flex flex flex-row flex-row rounded-xl bg-gray-300 py-1 px-3">
       <small>{text}</small>
+      <X className="text-xs" />
     </div>
   );
 };
