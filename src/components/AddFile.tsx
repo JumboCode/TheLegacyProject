12a84791fba879209acf72d1815d1fd06943073c
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-04-07 10:07:58 -0400

    added padding to name and tag inputs and added hover effects to buttons

--- src/components/AddFile.tsx
+++ src/components/AddFile.tsx
@@ -1,37 +1,53 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const AddFile = () => {
-  const [visibility, setVisibility] = useState<boolean>(true)
+  const [visibility, setVisibility] = useState<boolean>(true);
 
   const handleCancel = () => {
+    // TODO: we should clear the form state here too
     setVisibility(!visibility);
-  }
+  };
 
   return (
-    <div className={`w-full h-full backdrop-blur-[2px] backdrop-brightness-75 absolute ${visibility ? '' : 'hidden'}`}>
-      <div className='bg-[#FFFFFF] w-[500px] h-[700px] top-0 bottom-0 left-0 right-0 m-auto absolute rounded-[10px] flex flex-col px-[31px]'>
+    <div
+      className={`absolute h-full w-full backdrop-blur-[2px] backdrop-brightness-75 ${
+        visibility ? "" : "hidden"
+      }`}
+    >
+      <div className="absolute top-0 bottom-0 left-0 right-0 m-auto flex h-[700px] w-[500px] flex-col rounded-[10px] bg-[#FFFFFF] px-[31px]">
+        <span className="mt-[41px] h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+          File name
+        </span>
+        <input className="mt-[15px] h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
 
-        <span className='w-full h-[34px] mt-[41px] text-sm leading-[22px] text-dark-gray font-sans'>File name</span>
-        <input className='mt-[15px] rounded w-full h-[46px] border-[#e6e6e6] border-solid border-[0.3px]' />
-
-        <span className='w-full h-[34px] text-sm mt-[22px] leading-[22px] text-dark-gray'>Description</span>
-        <textarea className='mt-[15px] p-[12px] rounded w-full h-[123px] border-[#e6e6e6] border-solid border-[0.3px] bg-[#F5F6FA] text-sm text-start' 
-          placeholder='Write a detailed description' 
+        <span className="mt-[22px] h-[34px] w-full text-sm leading-[22px] text-dark-gray">
+          Description
+        </span>
+        <textarea
+          className="mt-[15px] h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-sm"
+          placeholder="Write a detailed description"
         />
 
         {/* not sure the correct tag to use for tag inputs... */}
-        <span className='w-full h-[34px] mt-[22px] text-sm leading-[22px] text-dark-gray font-sans'>Tags</span>
-        <input className='mt-[15px] rounded w-full h-[46px] border-[#e6e6e6] border-solid border-[0.3px]' />
+        <span className="mt-[22px] h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+          Tags
+        </span>
+        <input className="mt-[15px] h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
 
-        <div className='w-[300px] h-[54px] flex flex-row mt-[154px] m-auto gap-4'>
-          <button className='w-full text-sm bg-off-white text-[#515151] font-normal rounded'
-            onClick={handleCancel}>Cancel</button>
-          <button className='w-full text-sm bg-dark-teal text-[#FFFFFF] font-normal rounded'>Create</button>
+        <div className="m-auto mt-[154px] flex h-[54px] w-[300px] flex-row gap-4">
+          <button
+            className="w-full rounded bg-off-white text-sm font-normal text-[#515151] hover:bg-gray-200"
+            onClick={handleCancel}
+          >
+            Cancel
+          </button>
+          <button className="w-full rounded bg-teal-800 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal">
+            Create
+          </button>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default AddFile;
-
