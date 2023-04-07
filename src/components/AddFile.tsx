f496557b463f34ea98630d7be48a404beeebaa67
Amitav Nott <58123848+anott03@users.noreply.github.com>
2023-04-07 13:22:55 -0400

    working on a styling implementation refactor

--- src/components/AddFile.tsx
+++ src/components/AddFile.tsx
@@ -1,5 +1,34 @@
 import React, { useState } from "react";
 
+const Tag = ({ text }: { text: string }) => {
+  return (
+    <div className="rounded-xl bg-gray-300 py-1 px-3">
+      <small>{text}</small>
+    </div>
+  );
+};
+
+const TagSelector = () => {
+  const [selectedTags, setSelectedTags] = useState<string[]>(["tag 3"]);
+  const tagList = ["tag 1", "tag 2", "tag 3"];
+
+  return (
+    <div>
+      <span className="h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+        Tags
+      </span>
+      <form className="mt-[1rem]">
+        <input className="h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
+      </form>
+      <div className="flex flex-row p-3">
+        {selectedTags.map((tag: string) => (
+          <Tag text={tag} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
 const AddFile = () => {
   const [visibility, setVisibility] = useState<boolean>(true);
 
@@ -9,44 +38,42 @@ const AddFile = () => {
   };
 
   return (
-    <div
-      className={`absolute h-full w-full backdrop-blur-[2px] backdrop-brightness-75 ${
-        visibility ? "" : "hidden"
-      }`}
-    >
-      <div className="absolute top-0 bottom-0 left-0 right-0 m-auto flex h-[700px] w-[500px] flex-col rounded-[10px] bg-[#FFFFFF] px-[31px]">
-        <span className="mt-[41px] h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
-          File name
-        </span>
-        <input className="mt-[15px] h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
+    <>
+      {visibility && (
+        <>
+          <div className="absolute flex h-full w-full flex-row items-center justify-center backdrop-blur-[2px] backdrop-brightness-75">
+            <div className="flex h-[700px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
+              <div>
+                <span className="my-5 h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+                  File name
+                </span>
+                <input className="my-5 h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
+                <span className="mt-[1rem] h-[34px] w-full text-sm leading-[22px] text-dark-gray">
+                  Description
+                </span>
+                <textarea
+                  className="my-5 h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-sm"
+                  placeholder="Write a detailed description"
+                />
+                <TagSelector />
+              </div>
 
-        <span className="mt-[22px] h-[34px] w-full text-sm leading-[22px] text-dark-gray">
-          Description
-        </span>
-        <textarea
-          className="mt-[15px] h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-sm"
-          placeholder="Write a detailed description"
-        />
-
-        {/* not sure the correct tag to use for tag inputs... */}
-        <span className="mt-[22px] h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
-          Tags
-        </span>
-        <input className="mt-[15px] h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" />
-
-        <div className="m-auto mt-[154px] flex h-[54px] w-[300px] flex-row gap-4">
-          <button
-            className="w-full rounded bg-off-white text-sm font-normal text-[#515151] hover:bg-gray-200"
-            onClick={handleCancel}
-          >
-            Cancel
-          </button>
-          <button className="w-full rounded bg-teal-800 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal">
-            Create
-          </button>
-        </div>
-      </div>
-    </div>
+              <div className="flex w-full flex-row justify-center">
+                <button
+                  className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </button>
+                <button className="mx-1 w-full max-w-[10rem] rounded bg-teal-800 p-3 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal">
+                  Create
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
+    </>
   );
 };
 
