4259cc226cc9175e5dbc018a8932a49f9ed5505b
Michael Simpson <michael.simpson@tufts.edu>
2023-04-08 12:23:53 -0400

    add auth checks and zod to /seniors/add

--- src/pages/api/seniors/add.tsx
+++ src/pages/api/seniors/add.tsx
@@ -1,33 +1,61 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@server/db/client";
 import { z } from "zod";
-
+import { getServerAuthSession } from "@server/common/get-server-auth-session";
 
 const add = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerAuthSession({ req, res });
+
+  if (!session) {
+    res.status(401).json({
+      error: "This route is protected. In order to access it, please sign in.",
+    });
+    return;
+  }
+
   switch (req.method) {
     case "POST":
       try {
-        const bodySchema = z.object({
-          name: z.string(),
-          location: z.string(),
-          description: z.string(),
-          students: z.array(z.string()),
-        });
+        const { admin } = (await prisma.user.findUnique({
+          where: {
+            id: session.user?.id,
+          },
+          select: {
+            admin: true,
+          },
+        })) ?? { admin: false };
 
-        const body = bodySchema.parse(JSON.parse(req.body));
+        if (admin) {
+          const bodySchema = z.object({
+            name: z.string(),
+            location: z.string(),
+            description: z.string(),
+            students: z.array(z.string()),
+          });
 
-        const senior = await prisma.senior.create({
-          data: {
-            name: body.name,
-            location: body.location,
-            description: body.description,
-            StudentIDs: body.students,
-            folder: `THIS NEEDS TO BE FIXED ONCE A FOLDER IS CRREATED YUP`
-          }
-        });
-      } catch {
+          const body = bodySchema.parse(JSON.parse(req.body));
+
+          const senior = await prisma.senior.create({
+            data: {
+              name: body.name,
+              location: body.location,
+              description: body.description,
+              StudentIDs: body.students,
+              folder: `THIS NEEDS TO BE FIXED ONCE A FOLDER IS CRREATED YUP`,
+            },
+          });
+
+          res.status(200).json(senior);
+        } else {
+          res.status(500).json({
+            error:
+              "This route is protected. In order to access it, please sign in as admin.",
+          });
+          return;
+        }
+      } catch (error) {
         res.status(500).json({
-          error: `failed to create senior`,
+          error: `failed to create senior: ${error}`,
         });
       }
       break;
@@ -38,41 +66,6 @@ const add = async (req: NextApiRequest, res: NextApiResponse) => {
       });
       break;
   }
-}
-
-const senior = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method = "POST") {
-    try {
-      const body = JSON.parse(req.body);
-      if (body) { // TODO: check if fields are well formed
-        const senior = await prisma.senior.create({
-          data: { 
-            id = body.id,
-            name = body.name,
-            location = body.location,
-            description = body.description,
-            photo = body.photo,
-            StudentIDs = body.StudentIDs,
-            Students = body.Students,
-            Files = body.Files,
-            Tags = body.Tags,
-          }
-        }) 
-      } else {
-        // throw new Error("One or more of the values is not well-defined.");
-      }                
-    };
-      res.status(200).json(senior);
-    } catch (error) {
-      res.status(500).json({
-      error: `failed to create senior`,
-      });
-    }
-  } else {
-    res.status(500).json({
-      error: `method ${req.method} not implemented`,
-    });
-  }
 };
 
-export default senior;
+export default add;
