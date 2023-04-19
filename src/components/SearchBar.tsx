eff14e568dc41322f9c3d09fa1325a814e91566e
Michael Simpson <michael.simpson@tufts.edu>
2023-04-19 17:29:24 -0400

    add search bar coponent

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,28 @@
+import { useCallback, useState } from "react";
+
+export interface ISearchBarProps {
+  setFilter: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SearchBar({ setFilter }: ISearchBarProps) {
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = useCallback(
+    (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+      setFilter(e.target.value);
+    },
+    [setFilter]
+  );
+
+  return (
+    <input
+      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900"
+      type="text"
+      placeholder="Search"
+      onChange={handleChange}
+      value={searchInput}
+    />
+  );
+}
--- src/pages/senior-profile.tsx
+++ src/pages/senior-profile.tsx
@@ -1,8 +1,16 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import { Dispatch, SetStateAction, useEffect, useRef, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import File, { FileProps, TagProps } from "@components/File";
-import DropdownCopy from "@components/dropdownCopy"
+import DropdownCopy from "@components/dropdownCopy";
+import SearchBar from "@components/SearchBar";
 
 type SeniorFields = {
   id: string;
@@ -14,77 +22,49 @@ type SeniorFields = {
   files: FileProps[];
 };
 
-export { SeniorFields };
+const fileArr: FileProps[] = [
+  {
+    id: "1",
+    name: "First Note",
+    description: "My first note",
+    lastModified: new Date(2023, 3, 7, 20, 34),
+    url: "/url",
+    tags: [
+      { name: "Childhood", color: "bg-tag-rust" },
+      { name: "Early career", color: "bg-tag-tan" },
+      { name: "Adulthood", color: "bg-tag-sage" },
+      { name: "Childhood", color: "bg-tag-rust" },
+      { name: "Early career", color: "bg-tag-tan" },
+      { name: "Adulthood", color: "bg-tag-sage" },
+    ],
+  },
 
-const SearchBar = ({
-  data,
-  setData,
-}: {
-  data: FileProps[];
-  setData: Dispatch<SetStateAction<FileProps[]>>;
-}) => {
-  const [searchInput, setSearchInput] = useState("");
-  const originalData = useRef(data);
-
-  const handleChange = (e: any) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-
-    if (searchInput.length === 0) {
-      setData(originalData.current);
-    } else if (searchInput.length > 0) {
-      //function to filter files
-      const filteredData = originalData.current.filter((x) => {
-        return x.name.includes(searchInput);
-      });
-      setData(filteredData);
-      //data.name.toLowerCase().includes(searchInput.toLowerCase());
-    }
-  };
-
-  return (
-    <input
-      className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-4 pl-10 text-sm text-gray-900"
-      type="text"
-      placeholder="Search"
-      onChange={handleChange}
-      value={searchInput}
-    />
-  );
-};
+  {
+    id: "2",
+    name: "Second Note",
+    description: "My second note",
+    lastModified: new Date(2023, 2, 31, 14, 52),
+    url: "/url2",
+    tags: [
+      { name: "College", color: "bg-tag-rust" },
+      { name: "Romance", color: "bg-tag-tan" },
+    ],
+  },
+];
 
 const SeniorProfile: NextPage = (initSeniorData: SeniorFields) => {
-  const fileArr: FileProps[] = [
-    {
-      id: "1",
-      name: "First Note",
-      description: "My first note",
-      lastModified: new Date(2023, 3, 7, 20, 34),
-      url: "/url",
-      tags: [
-        {name: "Childhood", color: "bg-tag-rust"},
-        {name: "Early career", color: "bg-tag-tan"},
-        {name: "Adulthood", color: "bg-tag-sage"},
-      ],
-    },
+  const [fileData, setFileData] = useState<FileProps[]>(fileArr);
+  const [filter, setFilter] = useState("");
 
-    {
-      id: "2",
-      name: "Second Note",
-      description: "My second note",
-      lastModified: new Date(2023, 2, 31, 14, 52),
-      url: "/url2",
-      tags: [
-        {name: "College", color: "bg-tag-rust"},
-        {name: "Romance", color: "bg-tag-tan"},
-      ],
-    },
-  ];
+  const filteredFiles = useMemo(
+    () =>
+      fileData.filter(({ name }) =>
+        name.toLowerCase().includes(filter.toLowerCase())
+      ),
+    [fileData, filter]
+  );
 
-  // initSeniorData will be set by passed-in prop; here's a temporary one
-  // to use for this ticket
-
-  initSeniorData = {
+  const seniorData = {
     id: "0",
     name: "Skylar Gilfeather",
     location: "Somerville",
@@ -94,26 +74,33 @@ const SeniorProfile: NextPage = (initSeniorData: SeniorFields) => {
     files: fileArr,
   };
 
-  const [fileData, setFileData] = useState<FileProps[]>(fileArr);
-  const [seniorData, setSeniorData] = useState<SeniorFields>(initSeniorData);
-
   return (
     <div className="container flex min-h-screen flex-col p-8">
-      <h1 className="text-teal font-serif text-[3rem] leading-normal mb-8">
+      <h1 className="text-teal mb-8 font-serif text-[3rem] leading-normal">
         {" "}
         {seniorData.name}
       </h1>
-      <div className="flex flex-row align-middle justify-between space-x-3">
-              <SearchBar data={fileData as FileProps[]} setData={setFileData} />
-          <div className="relative z-10">
-            <DropdownCopy items={["By Name", "By Last Modified"]} bgColor="red" selected="Sort" setSelected={Dispatch<SetStateAction<string>>}/>
-          </div>
+      <div className="flex flex-row justify-between space-x-3 align-middle">
+        <SearchBar setFilter={setFilter} />
+        <div className="relative z-10">
+          <DropdownCopy
+            items={["By Name", "By Last Modified"]}
+            bgColor="red"
+            selected="Sort"
+            setSelected={Dispatch<SetStateAction<string>>}
+          />
+        </div>
       </div>
       {/* styling for a TileGrid-like grid */}
       <div className="mt-7 grid grid-cols-[repeat(auto-fill,_256px)] gap-10 text-center">
-        {fileData.map(({name, lastModified, url, tags}: FileProps) => (
-          <div> 
-            <File name={name} lastModified={lastModified} url={url} tags={tags}/> 
+        {filteredFiles.map(({ name, lastModified, url, tags }: FileProps) => (
+          <div key={url}>
+            <File
+              name={name}
+              lastModified={lastModified}
+              url={url}
+              tags={tags}
+            />
           </div>
         ))}
       </div>
