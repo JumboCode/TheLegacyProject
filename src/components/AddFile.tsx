e8f36f41f88e47b3d192021772ff457a9e669e83
Loc Mai <locmai0315@gmail.com>
2023-04-30 13:10:36 -0400

    confirm/error popup

--- src/components/AddFile.tsx
+++ src/components/AddFile.tsx
@@ -36,26 +36,26 @@ const Tag = ({ text }: { text: string }) => {
 };
 
 const TagSelector = () => {
-  const [selectedTags, setSelectedTags] = useState([
-    "tag 3",
-  ]);
+  const [selectedTags, setSelectedTags] = useState(["tag 3"]);
   const tagList = ["tag 1", "tag 2", "tag 3"];
-  const [ filterText, setFilterText ] = useState("");
+  const [filterText, setFilterText] = useState("");
 
   const filteredTags = useMemo(
-    () => tagList.filter(tag => tag.indexOf(filterText) != -1),
+    () => tagList.filter((tag) => tag.indexOf(filterText) != -1),
     [filterText]
   );
-  
+
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFilterText(e.target.value);
-  }
+  };
 
   return (
     <div>
-      <span className="h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray"> Tags
+      <span className="h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+        {" "}
+        Tags
       </span>
-      <input 
+      <input
         className="mt-[1rem] h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3"
         value={filterText}
         onChange={onChange}
@@ -70,45 +70,57 @@ const TagSelector = () => {
 };
 
 const AddFile = ({ showAddFilePopUp, setShowAddFilePopUp }: AddFileProps) => {
-  const [ fileName, setFilename ] = useState<string>("");
-  const [ description, setDescription ] = useState<string>("");
+  const [fileName, setFilename] = useState<string>("");
+  const [description, setDescription] = useState<string>("");
+  const [confirm, setConfirm] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
 
   const handleCancel = () => {
     setShowAddFilePopUp(!showAddFilePopUp);
   };
 
-  const addFile = () => {
-    fetch("/api/drive/addfile", {
+  const addFile = async () => {
+    const res = await fetch("/api/drive/addfile", {
       method: "POST",
       body: JSON.stringify({
         fileName: fileName,
         description: description,
       }),
-    })
-  }
+    });
+    if (res.status === 200) {
+      setConfirm(true);
+    } else {
+      setError(true);
+    }
+  };
 
   return (
     <>
       {showAddFilePopUp && (
-        <>
-          <div className="absolute flex h-full w-full flex-row items-center justify-center backdrop-blur-[2px] backdrop-brightness-75">
+        <div className="absolute flex h-full w-full flex-row items-center justify-center backdrop-blur-[2px] backdrop-brightness-75">
+          {!confirm && !error ? (
             <div className="flex h-[700px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
               <div>
                 <span className="my-5 h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
                   File name
                 </span>
-                <input className="my-5 h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3" 
-                  type="text" 
-                  value={fileName} 
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => setFilename(e.target.value)}
+                <input
+                  className="my-5 h-[46px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] px-3"
+                  type="text"
+                  value={fileName}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setFilename(e.target.value)
+                  }
                 />
                 <span className="mt-[1rem] h-[34px] w-full text-sm leading-[22px] text-dark-gray">
-                Description
+                  Description
                 </span>
                 <textarea
                   className="my-5 h-[123px] max-h-[123px] min-h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-sm"
                   placeholder="Write a detailed description"
-                  onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setDescription(e.target.value)}
+                  onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
+                    setDescription(e.target.value)
+                  }
                 />
                 <TagSelector />
               </div>
@@ -116,16 +128,48 @@ const AddFile = ({ showAddFilePopUp, setShowAddFilePopUp }: AddFileProps) => {
               <div className="flex w-full flex-row justify-center">
                 <button
                   className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
-                  onClick={handleCancel} >
-                  Cancel </button>
-                <button className="mx-1 w-full max-w-[10rem] rounded bg-teal-800 p-3 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal"
-                  onClick={addFile} >
+                  onClick={handleCancel}
+                >
+                  Cancel{" "}
+                </button>
+                <button
+                  className="mx-1 w-full max-w-[10rem] rounded bg-teal-800 p-3 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal"
+                  onClick={addFile}
+                >
                   Create
                 </button>
               </div>
             </div>
-          </div>
-        </>
+          ) : (
+            <>
+              {confirm ? (
+                <div className="flex h-[700px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
+                  <span>File added successfully!</span>
+                  <div className="flex w-full flex-row justify-center">
+                    <button
+                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                      onClick={() => setShowAddFilePopUp(false)}
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex h-[700px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
+                  <span>Error in adding file...</span>
+                  <div className="flex w-full flex-row justify-center">
+                    <button
+                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                      onClick={() => setShowAddFilePopUp(false)}
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                </div>
+              )}
+            </>
+          )}
+        </div>
       )}
     </>
   );
--- src/pages/api/drive/addfile.ts
+++ src/pages/api/drive/addfile.ts
@@ -1,20 +1,20 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { google } from 'googleapis';
-import { getServerAuthSession } from "@server/common/get-server-auth-session";
-import drive from '../drive/drive';
+// import { google } from 'googleapis';
+// import { getServerAuthSession } from "@server/common/get-server-auth-session";
+import drive from "../drive/drive";
 
 const uploadToFolder = async (req: NextApiRequest, res: NextApiResponse) => {
   const service = await drive(req, res);
 
   const fileData = JSON.parse(req.body);
-  const parentID = '1MVyWBeKCd1erNe9gkwBf7yz3wGa40g9a';
+  const parentID = "1MVyWBeKCd1erNe9gkwBf7yz3wGa40g9a";
   const fileMetadata = {
     name: [fileData.fileName],
-    mimeType: 'application/vnd.google-apps.document',
+    mimeType: "application/vnd.google-apps.document",
     parents: [parentID],
   };
   const media = {
-    mimeType: 'text/plain',
+    mimeType: "text/plain",
     body: fileData.description,
   };
 
@@ -22,26 +22,24 @@ const uploadToFolder = async (req: NextApiRequest, res: NextApiResponse) => {
     const file = await service.files.create({
       resource: fileMetadata,
       media: media,
-      fields: 'id',
+      fields: "id",
     });
-    console.log('Successfully created document with ID:', file.data.id);
+    console.log("Successfully created document with ID:", file.data.id);
 
     res.status(200).json({
       message: "Successfully created document",
       folder: parentID,
       fileID: file.data.id,
       file: fileData.fileName,
-    })
+    });
     return file.data.id;
   } catch (err) {
     // TODO(developer) - Handle error
     res.status(401).json({
-      error: err
-    })
+      error: err,
+    });
     throw err;
   }
-}
+};
 
 export default uploadToFolder;
-
-
