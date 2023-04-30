0d170904ba57986c500b1bd03cf3dc3cd8174b89
Alyssa Williams <alyssa.williams.tk360@gmail.com>
2023-04-30 14:53:39 -0400

    Adjusted some of the styling for pending page

--- src/pages/pending.tsx
+++ src/pages/pending.tsx
@@ -2,25 +2,27 @@ import Image from "next/image";
 
 export default function Pending() {
   return (
-    <main className="text-center m-auto text-white">
-    <div className="bg-dark-teal p-16 rounded-lg">
-      <h1 className="text-4xl font-bold">Pending</h1>
+    <main className="text-center m-auto">
+    <div className="p-16">
+      <h1 className="text-4xl font-bold font-serif">Pending</h1>
       <br></br>
-      <h1 className="text-2xl font-semibold italic">Uh Oh! Looks Like You Don't Have Access Yet.</h1>
+      <h1 className="text-2xl font-semibold italic font-serif">Uh Oh! Looks like you don't have access yet.</h1>
       <br></br>
       <h2>
         Your access to The Legacy Project is pending, please follow-up with a
         club administrator.
       </h2>
     </div>
+    <div className="">
     <Image
         src="/pending/orange dried flower thing.png"
         alt="A circular photo of younger woman helping a senior citizen"
         height={300}
         width={300}
         objectFit="cover"
-        className="absolute z-10 duration-500"
+        className="absolute z-10 opacity-80"
       ></Image>
+    </div>
     </main>
   );
 }
