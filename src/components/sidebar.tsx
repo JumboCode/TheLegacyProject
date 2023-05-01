38ae6a783d9c3224d2801ae8aa68860ddbb470f3
Michael Simpson <michaelsimpson2019@gmail.com>
2023-05-01 16:41:13 -0400

    add file streamline (#84)

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,24 +6,23 @@ import SignIn from "./SignIn";
 
 const Sidebar = () => {
   return (
-      <div className="flex flex-col h-screen sticky top-0 items-center w-7/8 justify-between bg-taupe px-6 py-8 shadow-lg drop-shadow-lg">
-        <div className="p-2 items-center w-full">
-          <div className="mb-10">
-            <h1 className="text-left font-serif text-3xl">The Legacy Project</h1>
-          </div>
-
-          <Link href="/index">
-            <button className="bg-white-500 w-full text-center rounded-xl bg-dark-green px-5 py-3 font-serif text-xl text-white hover:bg-dark-green-hover duration-150 hover:-translate-y-0.5">
-                Home
-            </button>
-          </Link>
+    <div className="w-7/8 sticky top-0 flex h-screen flex-col items-center justify-between bg-taupe px-6 py-8 shadow-lg drop-shadow-lg">
+      <div className="w-full items-center p-2">
+        <div className="mb-10">
+          <h1 className="text-left font-serif text-3xl">The Legacy Project</h1>
         </div>
 
-        <div className="p-2 items-center w-full">
-          <SignIn />
-        </div>
-
+        <Link href="/home">
+          <button className="bg-white-500 hover:bg-dark-green-hover w-full rounded-xl bg-dark-green px-5 py-3 text-center font-serif text-xl text-white duration-150 hover:-translate-y-0.5">
+            Home
+          </button>
+        </Link>
       </div>
+
+      <div className="w-full items-center p-2">
+        <SignIn />
+      </div>
+    </div>
   );
 };
 
