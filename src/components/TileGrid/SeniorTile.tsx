caa2d4859d325767067781e9a6743227d0b6ec87
Michael Simpson <michaelsimpson2019@gmail.com>
2023-05-02 10:18:06 -0400

    Change student card rendering, make framework for admin promotion/demotion, and update filmcard modified date (#86)
    * update edit
    
    * date updates when file is clicked

--- src/components/TileGrid/SeniorTile.tsx
+++ src/components/TileGrid/SeniorTile.tsx
@@ -17,19 +17,27 @@ export function SeniorTile({
     <div className="p4 relative flex h-64 w-64 flex-col items-center justify-center gap-4 rounded bg-white text-base font-medium text-gray-700 drop-shadow-md">
       <div className="absolute top-2 right-2">
         <TileEdit
-          handleEdit={(e) => {
-            e.stopPropagation();
-            e.preventDefault();
-          }}
-          handleDelete={(e) => {
-            e.stopPropagation();
-            e.preventDefault();
-            fetch(`/api/senior/${senior.id}`, {
-              method: "DELETE",
-            });
-            setSeniors((prev) => prev.filter((s) => s.id !== senior.id));
-            refreshData();
-          }}
+          options={[
+            {
+              name: "Edit",
+              onClick: (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+              },
+            },
+            {
+              name: "Delete",
+              onClick: (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                fetch(`/api/senior/${senior.id}`, {
+                  method: "DELETE",
+                });
+                setSeniors((prev) => prev.filter((s) => s.id !== senior.id));
+                refreshData();
+              },
+            },
+          ]}
         />
       </div>
       <Link href={`/senior/${senior.id}`}>{senior.name}</Link>
