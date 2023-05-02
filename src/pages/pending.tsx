13eda07de1511e1376bbae891fd759648104b5d5
Alyssa Williams <alyssa.williams.tk360@gmail.com>
2023-05-02 18:11:26 -0400

    Could not figure out the gap issue with photoCarousel without messing up the rest of the functionality

--- src/pages/pending.tsx
+++ src/pages/pending.tsx
@@ -15,7 +15,7 @@ export default function Pending() {
     <div>
     <Image
         src="/pending/orange dried flower thing.png"
-        alt="A circular photo of younger woman helping a senior citizen"
+        alt="A dried orange flower"
         height={300}
         width={300}
         objectFit="cover"
