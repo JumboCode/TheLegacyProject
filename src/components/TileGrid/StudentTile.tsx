397ab638678156d61075000e2cc51236867c4c03
Michael Simpson <michael.simpson@tufts.edu>
2023-05-02 10:35:08 -0400

    add promotion and demotion

--- src/components/TileGrid/StudentTile.tsx
+++ src/components/TileGrid/StudentTile.tsx
@@ -17,27 +17,47 @@ export function StudentTile({
   refreshData,
 }: IStudentTileProps) {
   const { data } = useSession();
+
+  const options = [];
+  if (data && data?.user?.id !== student.id) {
+    if (student.admin) {
+      options.push({
+        name: "Demote",
+        onClick: () => {
+          setStudents((prev) =>
+            prev.map((s) => (s.id === student.id ? { ...s, admin: false } : s))
+          );
+          fetch(`/api/student/${student.id}/demote`, { method: "POST" });
+          refreshData();
+        },
+      });
+    } else {
+      options.push({
+        name: "Promote",
+        onClick: () => {
+          setStudents((prev) =>
+            prev.map((s) => (s.id === student.id ? { ...s, admin: true } : s))
+          );
+          fetch(`/api/student/${student.id}/promote`, { method: "POST" });
+          refreshData();
+        },
+      });
+    }
+
+    options.push({
+      name: "Deactivate",
+      onClick: () => {
+        setDeactivated((prev) => [...prev, student]);
+        setStudents((prev) => prev.filter((s) => s.id !== student.id));
+        refreshData();
+      },
+    });
+  }
+
   return (
     <div className="p4 relative flex h-64 w-64 flex-col items-center justify-center gap-4 rounded bg-white text-base font-medium text-gray-700 drop-shadow-md">
       <div className="absolute top-2 right-2">
-        <TileEdit
-          options={
-            !data || data?.user?.id === student.id
-              ? []
-              : [
-                  {
-                    name: "Deactivate",
-                    onClick: () => {
-                      setDeactivated((prev) => [...prev, student]);
-                      setStudents((prev) =>
-                        prev.filter((s) => s.id !== student.id)
-                      );
-                      refreshData();
-                    },
-                  },
-                ]
-          }
-        />
+        <TileEdit options={options} />
       </div>
       <div className="h-20 w-20 overflow-hidden rounded-full">
         <Image
