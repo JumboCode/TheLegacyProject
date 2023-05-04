6d494c7c7e2c5c195eb4bea308efb98ef0371322
Bowie Bello <obello26@icloud.com>
2023-05-04 00:08:32 -0400

    fixed responsiveness bug on join e list text also changed entry box focus color to match theme

--- src/components/emailSignup/emailSignupBox.tsx
+++ src/components/emailSignup/emailSignupBox.tsx
@@ -15,14 +15,14 @@ const EmailSignupBox = () => {
       <input
         type="email"
         id="email"
-        className="w-3/4 rounded-lg border border-gray-300 bg-taupe p-2.5 text-sm text-dark-gray focus:border-blue-500 focus:ring-blue-500"
+        className="w-3/4 rounded-lg border border-gray-300 bg-taupe p-2.5 text-sm text-dark-gray focus:border-dark-green focus:ring-dark-green"
         placeholder="john.doe@company.com"
         required
       />
       <input
         type="submit"
         value="Join E-List"
-        className="text-l w-1/4 rounded-lg bg-dark-green py-1.5 px-4 font-sans text-white duration-150 hover:-translate-y-0.5"
+        className="text-md w-fit rounded-lg bg-dark-green py-1.5 px-4 font-sans text-white duration-150 hover:-translate-y-0.5"
       ></input>
     </form>
   );
