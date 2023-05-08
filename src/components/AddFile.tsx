1e7e37b1630f39cddcdc46702d25f4695b78c429
Bowie Bello <obello26@icloud.com>
2023-05-08 01:04:50 -0400

    making text larger

--- src/components/AddFile.tsx
+++ src/components/AddFile.tsx
@@ -10,8 +10,8 @@ type AddFileProps = {
 
 const Tag = ({ text }: { text: string }) => {
   return (
-    <div className="my-0.5 mx-0.5 flex flex-row rounded-xl bg-gray-300 py-1 px-3">
-      <small>{text}</small>
+    <div className="my-0.5 mx-0.5 flex flex-row rounded-xl bg-gray-300 py-1 px-3 text-base">
+      <span>{text}</span>
     </div>
   );
 };
@@ -34,7 +34,7 @@ const TagSelector = ({
 
   return (
     <div>
-      <span className="h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+      <span className="h-[34px] w-full font-sans text-base leading-[22px] text-dark-gray">
         Tags
       </span>
       <FilterDropdown
@@ -101,7 +101,7 @@ const AddFile = ({
           {!confirm && !error ? (
             <div className="flex h-[700px] min-w-[400px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
               <div>
-                <span className="my-5 h-[34px] w-full font-sans text-sm leading-[22px] text-dark-gray">
+                <span className="my-5 h-[34px] w-full font-sans text-base leading-[22px] text-dark-gray">
                   File name
                 </span>
                 <input
@@ -112,11 +112,11 @@ const AddFile = ({
                     setFilename(e.target.value)
                   }
                 />
-                <span className="mt-[1rem] h-[34px] w-full text-sm leading-[22px] text-dark-gray">
+                <span className="mt-[1rem] h-[34px] w-full text-base leading-[22px] text-dark-gray">
                   Description
                 </span>
                 <textarea
-                  className="my-5 h-[123px] max-h-[123px] min-h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-sm"
+                  className="my-5 h-[123px] max-h-[123px] min-h-[123px] w-full rounded border-[0.3px] border-solid border-[#e6e6e6] bg-[#F5F6FA] p-[12px] text-start text-base"
                   placeholder="Write a detailed description"
                   onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) =>
                     setDescription(e.target.value)
@@ -130,13 +130,13 @@ const AddFile = ({
 
               <div className="flex w-full flex-row justify-center">
                 <button
-                  className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                  className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-base font-normal text-[#515151] hover:bg-gray-200"
                   onClick={handleCancel}
                 >
                   Cancel{" "}
                 </button>
                 <button
-                  className="mx-1 w-full max-w-[10rem] rounded bg-teal-800 p-3 text-sm font-normal text-[#FFFFFF] hover:bg-dark-teal"
+                  className="mx-1 w-full max-w-[10rem] rounded bg-teal-800 p-3 text-base font-normal text-[#FFFFFF] hover:bg-dark-teal"
                   onClick={addFile}
                 >
                   Create
@@ -150,7 +150,7 @@ const AddFile = ({
                   <span>File added successfully!</span>
                   <div className="flex w-full flex-row justify-center">
                     <button
-                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-base font-normal text-[#515151] hover:bg-gray-200"
                       onClick={() => setShowAddFilePopUp(false)}
                     >
                       Confirm
@@ -158,11 +158,11 @@ const AddFile = ({
                   </div>
                 </div>
               ) : (
-                <div className="flex h-[250px] max-w-[35%] flex-col justify-between rounded-lg bg-white p-10">
+                <div className="flex h-[250px] max-w-[35%] flex-col justify-between text-base rounded-lg bg-white p-10">
                   <span>Error in adding file...</span>
                   <div className="flex w-full flex-row justify-center">
                     <button
-                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-sm font-normal text-[#515151] hover:bg-gray-200"
+                      className="mx-1 w-full max-w-[10rem] rounded bg-off-white p-3 text-base font-normal text-[#515151] hover:bg-gray-200"
                       onClick={() => setShowAddFilePopUp(false)}
                     >
                       Confirm
