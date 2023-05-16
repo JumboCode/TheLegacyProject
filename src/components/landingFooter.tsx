a2893fde84fbd06f9b2009e5673a73bba139c612
Loc Mai <locmai0315@gmail.com>
2023-05-15 20:31:04 -0400

    contact us scroll to contact section on home

--- src/components/landingFooter.tsx
+++ src/components/landingFooter.tsx
@@ -5,32 +5,31 @@ import React from "react";
 const landingFooter = () => {
   return (
     <>
-      <div className="pb-9 w-[659px] text-center bg-taupe">
-        <p className="text-4xl font-bold font-serif text-[40px] px-[21.5px] leading-[56px]">
+      <div className="w-[659px] bg-taupe pb-9 text-center">
+        <p className="px-[21.5px] font-serif text-4xl text-[40px] font-bold leading-[56px]">
           The Legacy Project. We&apos;re here.
         </p>
         <br></br>
-        <p className="font-normal text-lg font-serif opacity-80">
+        <p className="font-serif text-lg font-normal opacity-80">
           We believe that the experiences and wisdom of our elders are valuable
           treasures that should be passed down to future generations. Join us in
-          our effort to honor the past and shape the future by preserving the 
-          stories of our elders</p>
+          our effort to honor the past and shape the future by preserving the
+          stories of our elders
+        </p>
         <br></br>
         <br></br>
-        <div className="flex flex-col gap-3">
+        <div className="flex flex-col gap-3" id="contact">
           <a href="mailto:arielle.galinsky@tufts.edu">
-            <button className="bg-light-sage py-1.5 px-5 text-md rounded-lg duration-150 hover:-translate-y-0.5">
-              ✉️  Email
+            <button className="text-md rounded-lg bg-light-sage py-1.5 px-5 duration-150 hover:-translate-y-0.5">
+              ✉️ Email
             </button>
           </a>
           <a href="https://www.instagram.com/tuftslegacyproject/">
-            <button className="bg-light-sage py-1.5 px-5 text-md rounded-lg duration-150 hover:-translate-y-0.5">
+            <button className="text-md rounded-lg bg-light-sage py-1.5 px-5 duration-150 hover:-translate-y-0.5">
               🌿 Instagram
             </button>
           </a>
-          
         </div>
-        
       </div>
     </>
   );
--- src/components/navbar/NavbarItem.tsx
+++ src/components/navbar/NavbarItem.tsx
@@ -6,10 +6,23 @@ interface NavbarItemProps {
   to: string;
 }
 
-const NavbarItem = ({ label, to }: NavbarItemProps) => (
-  <div className="text-2xl font-serif leading-normal text-dark-plum duration-150 hover:-translate-y-0.5 lg:mr-5">
-    <Link href={to}>{label}</Link>
-  </div>
-);
+const NavbarItem = ({ label, to }: NavbarItemProps) => {
+  const isBrowser = () => typeof window !== "undefined";
+
+  const scrollToContact = () => {
+    if (!isBrowser()) return;
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+  return (
+    <div className="font-serif text-2xl leading-normal text-dark-plum duration-150 hover:-translate-y-0.5 lg:mr-5">
+      <Link href={to} onClick={scrollToContact}>
+        {label}
+      </Link>
+    </div>
+  );
+};
 
 export default NavbarItem;
