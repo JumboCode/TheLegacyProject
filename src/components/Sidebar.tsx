095b85349e7ea117ac6c83711f53530d7e66ce39
Skylar Gilfeather <42349909+sgilfeather@users.noreply.github.com>
2023-08-08 13:51:43 -0400

    Skylar/student senior improvements (#105)
    * hooked up addsenior tile and senior profile tiles
    
    * added add file to senior/[id]
    
    * student-home -> student/[id], populated with that student's seniors
    
    * addfile stylistic tweaks
    
    * little navbar and button stylings
    
    * addsenior styling to match addfile
    
    * more addsenior styling and starting mobile
    
    * functional AddSenior, page updates immediately
    
    * can click both seniortile and dropdown
    
    * working senior delete; todo, delete folder in drive
    
    * update messages when opening addsenior from breadcrumbs
    
    * works but no update of grid
    
    * proper updating of senior tile, no sorting though
    
    * working student dropdown for addsenior
    
    * resolved tag types for tag dropdown
    
    * working tag types and tag dropdown
    
    * working student-senior relations
    
    * further home and landing page work
    
    * temporary fixes for working production build
    
    * removed package-lock.json

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import SignIn from "./SignIn";
+
+const Sidebar = ({ displayName } : { displayName: string }) => {
+  return (
+    // <div className="w-7/8 sticky top-0 flex h-screen flex-col items-center justify-between bg-med-tan px-6 py-8 shadow-lg drop-shadow-lg">
+    <div className="flex w-full sticky top-0 flex h-screen flex-col items-center justify-between bg-nav-taupe px-6 py-8 shadow-xl drop-shadow-xl">
+      <div className="w-full items-center p-2">
+        <div className="mb-10">
+          <Link href="/">
+            <h1 className="rounded p-4 bg-tan text-left font-serif text-3xl drop-shadow-md /
+                           duration-150 hover:-translate-y-0.5">
+              The Legacy Project
+            </h1>
+          </Link>
+        </div>
+
+        <Link href="/home">
+          <button className="w-full rounded bg-nav-teal px-5 py-3 text-center font-serif text-xl text-white \
+                             drop-shadow-md hover:bg-dark-teal duration-150 hover:-translate-y-0.5">
+            Home
+          </button>
+        </Link>
+      </div>
+
+      <div className="w-full items-center p-2">
+        <SignIn isPublic={displayName === "public"}/>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
