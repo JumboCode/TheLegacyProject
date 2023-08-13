8c11bec2d230f4153ac3828553fd5220188f745a
sgilfeather <skylar.gilfeather@gmail.com>
2023-08-13 19:45:37 -0400

    Quick layout of Our Story text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -109,6 +109,61 @@ const Home: NextPage = () => {
             </span>
           </div>
         <div className="flex flex-col text-wrap px-10 items-center w-full">
+        <div className="flex flex-col justify-center w-full gap-y-4 px-[20px]">
+          <span className="font-serif font-semibold text-center sm:text-left text-3xl sm:text-4xl">
+            About Us
+          </span>
+          <p className="tracking-easy">
+            The concept of The Legacy Project (TLP) began in 2018, when Arielle Galinsky, a high school junior at the
+            time, spent her time working at a local senior community in her Massachusetts hometown. Her role allowed her
+            to interact with the residents, which she greatly enjoyed, yet she always left her shift with a strong
+            desire to learn more about the stories of the seniors she met. With this strong curiosity, paired with a
+            deep sense of regret she felt having lost her own grandfathers at a young age, Arielle decided to spearhead
+            TLP as a personal project. The initiative encompassed conducting thorough interviews of 18 residents living
+            in the community, with the intention to hold a ceremony at the finale of the project to provide her older
+            friends with the opportunity to share their stories with their families. When the COVID-19 pandemic hit,
+            Arielle pivoted, and decided the best way to preserve the 18 captivating stories of the seniors she
+            interviewed was through prose. Arielle published Their Lives Reflected: A Treasury of Life Stories Captured
+            Through The Legacy Project in 2021 to showcase and celebrate their life accounts, both for the seniors
+            themselves and their families.
+          </p>
+          <p>
+            Little did she know that, in the state right next over, Katie Furey, a sophomore in college home for the
+            pandemic, started a similar project of her own. Katie had always been close with her grandmother, Mary,
+            who was living in a nursing home at the time of the pandemic. When Katie and her family would visit Mary
+            and her neighbors from outside the window of the nursing home, waving frantically through the glass and
+            chatting on the phone, Katie wished there was a way for young people like herself to connect more deeply
+            with these wonderful people on the other side of the glass who were experiencing a loneliness perhaps even
+            more pronounced than the loneliness Katie and her peers were facing as the pandemic ramped up.
+          </p>
+          <p>
+            The United Nations Millennium Fellowship being hosted through Tufts University presented a perfect
+            opportunity for Katie to put her idea into action. Using the program’s framework, Katie partnered with
+            the Little Sisters of the Poor home in Enfield, Connecticut. Katie met around ten older adults from the
+            home on Zoom, connecting with them and learning tidbits of their life stories, which she is working on
+            compiling as a series of vignettes.  
+          </p>
+          <p>
+            Arielle and Katie met in 2020, quickly connecting over their aligned passions to build intergenerational
+            connections provide a platform for local seniors to share their stories, and preserve such histories
+            through their collective love for writing. They decided to start the Tufts Legacy Project, a chapter of
+            The Legacy Project based out of Tufts University in Medford, Massachusetts which seeks to bond college
+            students 1:1 with older adults living in the host communities of Medford and Somerville. When Katie decided
+            to join the Millennium Fellowship for a second year, she met Wanda Schlumpf, a sophomore at Tufts, who was
+            similarly dedicated to the value of storytelling. Wanda took on the project with her and joined the Tufts
+            TLP chapter as a founding member. Together, the three women helped to build it into an organization of
+            nearly 30 students passionate about empowering local seniors with the platform to amplify their stories.
+          </p>
+          <p>
+            Having watched the Tufts chapter grow over the past two years, Arielle, Katie, and Wanda decided to establish
+            TLP as a nonprofit organization with the goal of spreading TLP’s mission across the country - preserving
+            stories of seniors who might not otherwise have the opportunity. TLP seeks to build sustainable,
+            intergenerational connections as a means to combat social isolation, preserve the life histories of older
+            adults, engage young individuals in revolutionizing perspectives on aging, and ensure that every old
+            adult- regardless of background - has the opportunity to share their experiences.
+          </p>
+        </div>
+
           <PhotoCarousel />
           <LandingFooter />
         </div>
