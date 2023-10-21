9100afdc44b2e3d90d10b3829b12177ebce85525
bellybubjoeOfficial <shanmugamganesan2004@gmail.com>
2023-10-21 19:03:46 -0400

    Validation logic WIP

--- src/app/api/chapter-request/route.schema.ts
+++ src/app/api/chapter-request/route.schema.ts
@@ -15,7 +15,7 @@ import { z } from "zod";
 export const ChapterRequest = z.object({
   firstName: z.string(),
   lastName: z.string(),
-  universityEmail: z.string(),
+  universityEmail: z.string().email("This is not a valid email"),
   phoneNumber: z.string(),
   university: z.string(),
   universityAddress: z.string(),
@@ -29,5 +29,56 @@ export const ChapterRequestResponse = z.discriminatedUnion("code", [
   z.object({
     code: z.literal("SUCCESS"),
     message: z.literal("Chapter request successfully submitted"),
+    data: z.any(),
+  }),
+  z.object({
+    code: z.literal("INVALID_FIRST_NAME"),
+    message: z.literal("Invalid first name provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_LAST_NAME"),
+    message: z.literal("Invalid last name provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_EMAIL"),
+    message: z.literal("Invalid email provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_PHONE_NUMBER"),
+    message: z.literal("Invalid phone number provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_UNIVERSITY"),
+    message: z.literal("Invalid university name provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_UNIVERSITY_ADDRESS"),
+    message: z.literal("Invalid university address provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_LEADERSHIP_EXPERIENCE"),
+    message: z.literal("Invalid leadership experience provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_MOTIVATION"),
+    message: z.literal("Invalid motivation provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_AVAILABILITY"),
+    message: z.literal("Invalid availability provided"),
+  }),
+  z.object({
+    code: z.literal("INVALID_QUESTION"),
+    message: z.literal("Invalid question provided"),
+  }),
+  z.object({
+    code: z.literal("UNKNOWN"),
+    message: z.literal("Invalid request, unknown error"),
+  }),
+  z.object({
+    code: z.literal("DUPLICATE_EMAIL"),
+    message: z.literal(
+      "A chapter request associated with this email already exists"
+    ),
   }),
 ]);
--- src/app/api/chapter-request/route.ts
+++ src/app/api/chapter-request/route.ts
@@ -1,12 +1,154 @@
 import { NextRequest, NextResponse } from "next/server";
 import { ChapterRequest, ChapterRequestResponse } from "./route.schema";
+import { prisma } from "@server/db/client";
 
 export const POST = async (request: NextRequest) => {
-  return NextResponse.json(
-    ChapterRequestResponse.parse({
-      code: "SUCCESS",
-      message: "Chapter request successfully submitted",
-    }),
-    { status: 205 }
-  );
+  // Validate the data in the request
+  // If the data is invalid, return a 400 response
+  // with a JSON body containing the validation errors
+  const chapterRequest = ChapterRequest.safeParse(await request.json());
+  if (!chapterRequest.success) {
+    const error = chapterRequest.error.format();
+    if (error.firstName) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_FIRST_NAME",
+          message: "Invalid first name provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.lastName) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_LAST_NAME",
+          message: "Invalid last name provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.universityEmail) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_EMAIL",
+          message: "Invalid email provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.phoneNumber) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_PHONE_NUMBER",
+          message: "Invalid phone number provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.university) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_UNIVERSITY",
+          message: "Invalid university name provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.universityAddress) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_UNIVERSITY_ADDRESS",
+          message: "Invalid university address provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.leadershipExperience) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_LEADERSHIP_EXPERIENCE",
+          message: "Invalid leadership experience provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.motivation) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_MOTIVATION",
+          message: "Invalid motivation provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.availabilities) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_AVAILABILITY",
+          message: "Invalid availability provided",
+        }),
+        { status: 400 }
+      );
+    }
+    if (error.questions) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "INVALID_QUESTION",
+          message: "Invalid question provided",
+        }),
+        { status: 400 }
+      );
+    } else {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "UNKNOWN",
+          message: "Invalid request, unknown error",
+        }),
+        { status: 500 }
+      );
+    }
+  } else {
+    const body = chapterRequest.data;
+    // Check if the email already exists
+    // console.log(prisma.chapterRequest);
+    const hasSameEmail = await prisma.chapterRequest.findFirst({
+      where: {
+        universityEmail: body.universityEmail,
+      },
+    });
+
+    if (hasSameEmail) {
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "DUPLICATE_EMAIL",
+          message:
+            "A chapter request associated with this email already exists",
+        }),
+        { status: 400 }
+      );
+    }
+    // If the data is valid, save it to the database via prisma client
+    const prismaResponse = await prisma.chapterRequest.create({
+      data: {
+        firstName: body.firstName,
+        lastName: body.lastName,
+        universityEmail: body.universityEmail,
+        phoneNumber: body.phoneNumber,
+        university: body.university,
+        universityAddress: body.universityAddress,
+        leadershipExperience: body.leadershipExperience,
+        motivation: body.motivation,
+        availabilities: body.availabilities,
+        questions: body.questions,
+      },
+    });
+    return NextResponse.json(
+      ChapterRequestResponse.parse({
+        code: "SUCCESS",
+        message: "Chapter request successfully submitted",
+        data: prismaResponse,
+      }),
+      { status: 200 }
+    );
+  }
 };
