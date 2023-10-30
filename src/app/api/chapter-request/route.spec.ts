2733836793b2e6c8d538970affd1d99b90401d88
cledi01 <caleb.gorrila@gmail.com>
2023-10-30 01:14:36 -0400

    Delete test, and restructure JSON validation

--- src/app/api/chapter-request/route.spec.ts
+++ src/app/api/chapter-request/route.spec.ts
@@ -1,6 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { ChapterRequestResponse, ChapterRequest } from "./route.schema";
-import { chapterRequest } from "./route.client";
+import { ChapterRequest } from "./route.schema";
 
 describe("ChapterRequest", () => {
   test("InvalidEmailThrowsError", () => {
@@ -19,29 +18,4 @@ describe("ChapterRequest", () => {
       })
     ).toThrowError("This is not a valid email");
   });
-
-  test("DuplicateEmailThrowsError", async () => {
-    const body = {
-      firstName: "Don't delete me",
-      lastName: "Don't delete me",
-      universityEmail: "donaldduck@gmail.com",
-      phoneNumber: "1234567890",
-      university: "University of Waterloo",
-      universityAddress: "123 University Ave",
-      leadershipExperience: "I have lots of leadership experience",
-      motivation: "I am very motivated",
-      availabilities: "I am available",
-      questions: "I have a question",
-    };
-    const response = await fetch("http://localhost:3000/api/chapter-request", {
-      method: "POST",
-      body: JSON.stringify(body),
-    });
-    const chapterRequestResponse = ChapterRequestResponse.parse(
-      await response.json()
-    );
-    expect(chapterRequestResponse.code === "DUPLICATE_EMAIL").toBe(true);
-  });
 });
-
-// TODO: Create tests to validate unique email for chapter requests
--- src/app/api/chapter-request/route.ts
+++ src/app/api/chapter-request/route.ts
@@ -7,67 +7,67 @@ export const POST = async (request: NextRequest) => {
   // If the data is invalid, return a 400 response
   // with a JSON body containing the validation errors
 
-  const chapterRequest = ChapterRequest.safeParse(
-    // Validate a proper JSON was passed in as well
-    await request.json().catch(() => {
+  // Validate a proper JSON was passed in as well
+  try {
+    const chapterRequest = ChapterRequest.safeParse(await request.json());
+    if (!chapterRequest.success) {
       return NextResponse.json(
         ChapterRequestResponse.parse({
-          code: "UNKNOWN",
-          message: "Unknown error received",
-        }),
-        { status: 500 }
-      );
-    })
-  );
-  if (!chapterRequest.success) {
-    return NextResponse.json(
-      ChapterRequestResponse.parse({
-        code: "INVALID_FORM",
-        message: "Invalid form submission",
-      }),
-      { status: 400 }
-    );
-  } else {
-    const body = chapterRequest.data;
-    // Check if the email already exists
-    const hasSameEmail = await prisma.chapterRequest.findFirst({
-      where: {
-        universityEmail: body.universityEmail,
-      },
-    });
-
-    if (hasSameEmail) {
-      return NextResponse.json(
-        ChapterRequestResponse.parse({
-          code: "DUPLICATE_EMAIL",
-          message:
-            "A chapter request associated with this email already exists",
+          code: "INVALID_FORM",
+          message: "Invalid form submission",
         }),
         { status: 400 }
       );
-    }
+    } else {
+      const body = chapterRequest.data;
+      // Check if the email already exists
+      const hasSameEmail = await prisma.chapterRequest.findFirst({
+        where: {
+          universityEmail: body.universityEmail,
+        },
+      });
 
-    // If the data is valid, save it to the database via prisma client
-    await prisma.chapterRequest.create({
-      data: {
-        firstName: body.firstName,
-        lastName: body.lastName,
-        universityEmail: body.universityEmail,
-        phoneNumber: body.phoneNumber,
-        university: body.university,
-        universityAddress: body.universityAddress,
-        leadershipExperience: body.leadershipExperience,
-        motivation: body.motivation,
-        availabilities: body.availabilities,
-        questions: body.questions,
-      },
-    });
+      if (hasSameEmail) {
+        return NextResponse.json(
+          ChapterRequestResponse.parse({
+            code: "DUPLICATE_EMAIL",
+            message:
+              "A chapter request associated with this email already exists",
+          }),
+          { status: 400 }
+        );
+      }
+
+      // If the data is valid, save it to the database via prisma client
+      await prisma.chapterRequest.create({
+        data: {
+          firstName: body.firstName,
+          lastName: body.lastName,
+          universityEmail: body.universityEmail,
+          phoneNumber: body.phoneNumber,
+          university: body.university,
+          universityAddress: body.universityAddress,
+          leadershipExperience: body.leadershipExperience,
+          motivation: body.motivation,
+          availabilities: body.availabilities,
+          questions: body.questions,
+        },
+      });
+      return NextResponse.json(
+        ChapterRequestResponse.parse({
+          code: "SUCCESS",
+          message: "Chapter request successfully submitted",
+        }),
+        { status: 200 }
+      );
+    }
+  } catch {
     return NextResponse.json(
       ChapterRequestResponse.parse({
-        code: "SUCCESS",
-        message: "Chapter request successfully submitted",
+        code: "UNKNOWN",
+        message: "Unknown error received",
       }),
-      { status: 200 }
+      { status: 500 }
     );
-  }
+  }  
 };
