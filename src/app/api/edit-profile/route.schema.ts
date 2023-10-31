616dd596802ccc52926cb814a81275a4d67ab79c
tylerrlin <tylerrlin@gmail.com>
2023-10-31 19:01:11 -0400

    prisma schema updates and edit-profile route added

---
+++ src/app/api/edit-profile/route.schema.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+import { Prisma } from "@prisma/client";
+
+/*
+  firstName     String
+  lastName      String
+  pronouns      String
+*/
+
+export const EditProfileRequest = z.object({
+  firstName: z.string(),
+  lastName: z.string(),
+  pronouns: z.string(),
+});
+
+export const EditProfileResponse = z.discriminatedUnion("code", [
+  z.object({
+    code: z.literal("SUCCESS"),
+    message: z.literal("Profile successfully updated"),
+  }),
+  z.object({
+    code: z.literal("INVALID_FORM"),
+    message: z.literal("Invalid form submission"),
+  }),
+  z.object({
+    code: z.literal("UNKNOWN"),
+    message: z.literal("Unknown error received"),
+  }),
+]);
