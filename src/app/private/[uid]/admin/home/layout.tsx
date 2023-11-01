5070f45a7da93847c4043237eafab334afd52ee8
wkim10 <wkim10@tufts.edu>
2023-10-31 21:42:43 -0400

    add styling for nav tabs
    Co-authored-by: sburchfield33 <sburchfield33@users.noreply.github.com>

--- src/app/private/[uid]/admin/home/layout.tsx
+++ src/app/private/[uid]/admin/home/layout.tsx
@@ -1,5 +1,6 @@
 import { HeaderContainer } from "@components/container/index";
 import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 interface IAdminHomeLayout {
   children: React.ReactNode;
@@ -12,6 +13,23 @@ const AdminHomeLayout = ({ children }: IAdminHomeLayout) => {
       headerIcon={faHouse}
       showHorizontalLine={false}
     >
+      <div style={{ width: "fit-content" }}>
+        <div className="flex gap-6">
+          <div>
+            <Link href="/chapters" className="text-xl text-dark-teal">
+              CHAPTERS
+            </Link>
+            <hr style={{ border: "1px solid black", width: "100%" }} />
+          </div>
+          <div>
+            <Link href="/resources" className="text-xl text-dark-teal">
+              RESOURCES
+            </Link>
+            <hr style={{ border: "1px solid black", width: "100%" }} />
+          </div>
+        </div>
+        <hr style={{ border: "0.5px solid black", width: "100%" }} />
+      </div>
       {children}
     </HeaderContainer>
   );
