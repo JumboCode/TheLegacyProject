ef6e336df9f617eb30590c42a64c4e90a4a2cc72
wu-ada <adawuboston@gmail.com>
2023-11-06 17:52:30 -0500

    Begin work on new page

--- src/app/private/[uid]/admin/pending-chapters/page.tsx
+++ src/app/private/[uid]/admin/pending-chapters/page.tsx
@@ -1,5 +1,17 @@
-const PendingChapters = () => {
-  return null;
+import Button from "@components/Button";
+import { prisma } from "@server/db/client";
+
+const PendingChapters = async () => {
+  // Use prisma to get all pending chapter requests
+  console.log(prisma);
+  const pendingChapters = await prisma.user.findMany();
+  console.log(pendingChapters);
+  // Map every chapter request to a pending chapter component and return!
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
 };
 
 export default PendingChapters;
