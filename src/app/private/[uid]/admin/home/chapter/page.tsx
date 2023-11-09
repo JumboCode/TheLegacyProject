94df60706832363c3f3c5d487e6e6ca16f8c5d5a
Nathan Edwards <nathanieljesseedwards@gmail.com>
2023-11-09 11:39:47 -0500

    started on path structure

--- src/app/private/[uid]/admin/home/chapter/page.tsx
+++ src/app/private/[uid]/admin/home/chapter/page.tsx
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 const DisplayChapterPage = () => {
   return null;
 };
 
 export default DisplayChapterPage;
-=======
-const AdminHomePage = () => {
-  return null;
-};
-
-export default AdminHomePage;
->>>>>>> 74df88e (started on path structure)
