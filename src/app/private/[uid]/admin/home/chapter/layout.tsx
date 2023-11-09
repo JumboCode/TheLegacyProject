74df88e56fee2c3f0ebb5443b3778be0f685be8a
Nathan Edwards <nathanieljesseedwards@gmail.com>
2023-11-09 11:39:47 -0500

    started on path structure

---
+++ src/app/private/[uid]/admin/home/chapter/layout.tsx
@@ -0,0 +1,20 @@
+import { HeaderContainer } from "@components/container/index";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+
+interface IAdminHomeLayout {
+  children: React.ReactNode;
+}
+
+const AdminHomeLayout = ({ children }: IAdminHomeLayout) => {
+  return (
+    <HeaderContainer
+      header="Home"
+      headerIcon={faHouse}
+      showHorizontalLine={false}
+    >
+      {children}
+    </HeaderContainer>
+  );
+};
+
+export default AdminHomeLayout;
--- src/pages/admin-home.tsx
+++ src/pages/admin-home.tsx
@@ -19,6 +19,7 @@ import SortDropdown, { SortMethod } from "@components/SortDropdown";
 import cn from "classnames";
 import { prisma } from "@server/db/client";
 import PendingCard from "@components/PendingCard";
+import PathNav from "@components/PathNav";
 
 type IAdminProps = Awaited<ReturnType<typeof getServerSideProps>>["props"] & {
   redirect: undefined;
@@ -129,6 +130,7 @@ function StudentBody({
         <SearchBar setFilter={setFilter} />
         <SortDropdown sortMethod={sortMethod} setSortMethod={setSortMethod} />
       </div>
+      <PathNav />
       <TileGrid>
         {students
           .filter(({ name }) => name?.includes(filter))
