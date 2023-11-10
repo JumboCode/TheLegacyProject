9a987a23ba7718a97ff28d402123f57043b2a4b0
selenaz03 <selenazhu03@gmail.com>
2023-11-09 23:39:51 -0500

    comment out temporary code

--- src/app/private/[uid]/admin/home/chapter/layout.tsx
+++ src/app/private/[uid]/admin/home/chapter/layout.tsx
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 "use client";
 
+<<<<<<< HEAD
 import TileGrid, { StudentTile } from "@components/TileGrid";
 // import { useRouter } from "next/navigation";
 import { prisma } from "@server/db/client";
@@ -71,20 +72,44 @@ const AdminHomeLayout = ({ children }: IAdminHomeLayout) => {
         <div className="flex flex-row text-start">
           <div>Location: </div>
           <div className="ml-2 font-bold">Medford, MA</div>
+=======
+const ChapterLayout = () =>
+  // {
+  //   students,
+  //   setDeactivated,
+  //   setStudents,
+  //   refreshData,
+  // }: {
+  //   students: User[];
+  //   setDeactivated: Dispatch<SetStateAction<User[]>>;
+  //   setStudents: Dispatch<SetStateAction<User[]>>;
+  //   refreshData: () => void;
+  // }
+  {
+    return (
+      <>
+        <div className="font-merriweather mt-5 text-2xl font-bold">
+          Tufts University
+>>>>>>> c8a9cbc (comment out temporary code)
         </div>
-        <div className="flex flex-row text-start">
-          <div>No. of members: </div>
-          <div className="ml-2 font-bold">12</div>
+        <div className="font-merriweather mt-3 flex h-1/5 w-5/6 flex-col justify-between rounded-md bg-white p-5">
+          <div className="flex flex-row text-start">
+            <div>Location: </div>
+            <div className="ml-2 font-bold">Medford, MA</div>
+          </div>
+          <div className="flex flex-row text-start">
+            <div>No. of members: </div>
+            <div className="ml-2 font-bold">12</div>
+          </div>
+          <div className="flex flex-row text-start">
+            <div>Years Active: </div>
+            <div className="ml-2 font-bold">1</div>
+          </div>
         </div>
-        <div className="flex flex-row text-start">
-          <div>Years Active: </div>
-          <div className="ml-2 font-bold">1</div>
+        <div className="font-merriweather mt-5 text-xl font-bold">
+          Executive Board
         </div>
-      </div>
-      <div className="font-merriweather mt-5 text-xl font-bold">
-        Executive Board
-      </div>
-      {/* <div>
+        {/* <div>
         <TileGrid>
           {students.map((student) => (
             <div className="h-auto w-auto" key={student.id}>
@@ -99,8 +124,8 @@ const AdminHomeLayout = ({ children }: IAdminHomeLayout) => {
           ))}
         </TileGrid>
       </div> */}
-    </>
-  );
-};
+      </>
+    );
+  };
 
 export default AdminHomeLayout;
