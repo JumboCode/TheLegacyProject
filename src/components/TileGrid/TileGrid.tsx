111b9738ec615fa097fd0d22ac1a5ffce576653f
wkim10 <wkim10@tufts.edu>
2023-11-12 13:51:42 -0500

    small UI changes

--- src/components/TileGrid/TileGrid.tsx
+++ src/components/TileGrid/TileGrid.tsx
@@ -5,7 +5,7 @@ interface ITileGridProps {
 export default function TileGrid({ children }: ITileGridProps) {
   return (
     <div
-      className="\ mx-0 mx-8 mt-6 grid grid-cols-2 gap-12 pr-8 sm:grid-cols-3
+      className="\ mx-8 mt-6 grid grid-cols-2 gap-12 pr-8 sm:grid-cols-3
                     md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6"
     >
       {children}
