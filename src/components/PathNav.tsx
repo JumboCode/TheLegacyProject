56952ca748b62c0eb5c4360469f4287c188ed073
Nathan Edwards <nathanieljesseedwards@gmail.com>
2023-11-15 22:36:21 -0500

    mostly got pathnav working

--- src/components/PathNav.tsx
+++ src/components/PathNav.tsx
@@ -63,47 +63,44 @@ import TestPage from "../app/private/[uid]/admin/home/chapter/page";
 =======
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import TestPage from "../app/private/[uid]/admin/home/chapter/page";
+import TestPage from "../app/private/[uid]/admin/home/page";
 
 const PathNav = () => {
   return (
-    <Router>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/test">Test</Link>
-          </li>
-          {/* <li>
-            <Link to="/"></Link>
-          </li> */}
-        </ul>
-      </nav>
-      <Routes>
-        <Route path="/test" element={<TestPage />} />
-      </Routes>
-    </Router>
+    <>
+      <div className="font-merriweather mt-7 flex flex-row">
+        <div>Chapter</div>
+        <div className="px-2">&gt;</div>
+        <div className="text-dark-teal">Tufts University</div>
+      </div>
+    </>
   );
 };
 export default PathNav;
 
-// const Navigation = () => {
-//   return (
-//     <Router>
-//       <nav>
-//         <ul>
-//           <li>
-//             <Link to="/">Home</Link>
-//           </li>
-//           {/* <li>
-//             <Link to="/"></Link>
-//           </li> */}
-//         </ul>
-//       </nav>
-//       <Routes>
-//         <Route path="/" element={<HomePage />} />
-//         <Route path="/about" element={<AboutPage />} />
-//         <Route path="/contact" element={<ContactPage />} />
-//       </Routes>
-//     </Router>
-//   );
-// };
+// // const Navigation = () => {
+// //   return (
+// //     <Router>
+// //       <nav>
+// //         <ul>
+// //           <li>
+// //             <Link to="/">Home</Link>
+// //           </li>
+// //           <li>
+// //             <Link to="/about">About</Link>
+// //           </li>
+// //           <li>
+// //             <Link to="/contact">Contact</Link>
+// //           </li>
+// //         </ul>
+// //       </nav>
+
+// //       {/* Define your routes and corresponding components */}
+// //       <Routes>
+// //         <Route path="/" element={<HomePage />} />
+// //         <Route path="/about" element={<AboutPage />} />
+// //         <Route path="/contact" element={<ContactPage />} />
+// //       </Routes>
+// //     </Router>
+// //   );
+// // };
