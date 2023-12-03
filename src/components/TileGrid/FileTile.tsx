d81c834ddd8e115392c81e02e16cdb784d4e2cc9
nickbar01234 <nickbar01234@gmail.com>
2023-12-03 14:48:37 -0500

    Remove last-modified

--- src/components/TileGrid/FileTile.tsx
+++ src/components/TileGrid/FileTile.tsx
@@ -19,48 +19,6 @@ const FileTile = ({
   url,
   Tags,
 }: IFileTileProps) => {
-  const [lastModified, setLastModified] = useState(intialLastModified);
-
-  const timeOptions: Intl.DateTimeFormatOptions = {
-    hour: "numeric",
-    hour12: true,
-    minute: "numeric",
-  };
-  const dateOptions: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  };
-
-  const currentDate = new Date();
-  let dateString = "";
-
-  if (
-    lastModified.getFullYear() == currentDate.getFullYear() &&
-    lastModified.getMonth() == currentDate.getMonth()
-  ) {
-    if (
-      lastModified.getDay() == currentDate.getDay() &&
-      lastModified.getHours() <= currentDate.getHours()
-    ) {
-      // Today
-      dateString =
-        "today at " + lastModified.toLocaleTimeString(undefined, timeOptions);
-    }
-    if (
-      lastModified.getDay() == currentDate.getDay() - 1 &&
-      lastModified.getHours() > currentDate.getHours()
-    ) {
-      // Yesterday
-      dateString =
-        "yesterday at " +
-        lastModified.toLocaleTimeString(undefined, timeOptions);
-    }
-  } else {
-    // All other times
-    dateString = lastModified.toLocaleDateString(undefined, dateOptions);
-  }
-
   return (
     <Link href={url}>
       <div className="flex h-56 w-48 flex-col items-start gap-y-2.5 rounded-lg bg-dark-teal px-4 py-6">
@@ -78,39 +36,6 @@ const FileTile = ({
         ))}
       </div>
     </Link>
-    //     <div className="relative w-auto flex flex-col flex-nowrap aspect-square justify-center items-center rounded \  bg-white hover:bg-off-white text-base drop-shadow-md">
-    // <div className="flex aspect-square flex-col rounded bg-white p-8 text-left font-sans drop-shadow-md hover:cursor-pointer hover:bg-off-white">
-    //   <Link href={url ?? "/home"}>
-    //     <div className="flex flex-col">
-    //       <span className="mb-2 text-lg font-semibold"> {name} </span>
-    //       <a
-    //         href={url}
-    //         rel="noopener noreferrer"
-    //         target="_blank"
-    //         onClick={async () => {
-    //           const date = new Date();
-    //           setLastModified(date);
-    //           const res = await fetch(`/api/file/${id}/update`, {
-    //             method: "POST",
-    //           });
-    //           const newFile = await res.json();
-    //           if (newFile.error) {
-    //             console.error(newFile.error);
-    //             return;
-    //           }
-    //           setLastModified(new Date(newFile.lastModified));
-    //         }}
-    //       ></a>
-    //       <span> Opened {dateString} </span>
-    //       {/* Row of Tags */}
-    //       <div className="justify-left mt-20 flex flex-row gap-x-2 overflow-scroll">
-    //         {Tags.map((name, i) => (
-    //           <Tag key={i} name={name} color={tagMap.get(name) ?? ""} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </Link>
-    // </div>
   );
 };
 
