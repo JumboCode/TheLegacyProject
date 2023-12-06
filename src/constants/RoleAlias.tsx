0386248cfa523d86dcdf4166e9392c41a4e0e8d2
nickbar01234 <nickbar01234@gmail.com>
2023-12-05 23:07:19 -0500

    Use string instead of String

--- src/constants/RoleAlias.tsx
+++ src/constants/RoleAlias.tsx
@@ -1,6 +1,6 @@
 import { Role } from "@prisma/client";
 
-export const RoleAlias: Record<Role, String> = {
+export const RoleAlias: Record<Role, string> = {
   ADMIN: "Admin",
   USER: "User",
   CHAPTER_LEADER: "Chapter Leader",
