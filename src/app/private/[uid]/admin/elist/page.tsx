84903f62497f902b207a48fe829d0666de768aac
wkim10 <wkim10@tufts.edu>
2023-12-05 19:48:25 -0500

    finish first implementation
    Co-authored-by: sburchfield33 <sburchfield33@users.noreply.github.com>

--- src/app/private/[uid]/admin/elist/page.tsx
+++ src/app/private/[uid]/admin/elist/page.tsx
@@ -1,32 +1,52 @@
-"use client";
-
+import CSVSaveButton from "@components/CSVSaveButton";
+import { HeaderContainer } from "@components/container";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { Email } from "@prisma/client";
 import { prisma } from "@server/db/client";
-import { useState, useEffect } from "react";
-// import { useEffect, useState } from "react";
 
-const ElistPage = () => {
-  const [elist, setElist] = useState<Email[]>();
+const ElistPage = async () => {
+  const elistData = await prisma.email.findMany();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const elistData = await prisma.email.findMany();
-        setElist(elistData);
-      } catch (error) {
-        console.error("Error fetching elist:", error);
-      }
-    };
+  const chunkArray = (arr: Email[], size: number) => {
+    return Array.from({ length: Math.ceil(arr.length / size) }, (_, index) =>
+      arr.slice(index * size, index * size + size)
+    );
+  };
 
-    fetchData();
-  }, []);
+  const columns = chunkArray(elistData, 20);
 
   return (
     <div>
-      {elist &&
-        elist.map((elistEmail, index) => (
-          <div key={index}>{elistEmail.email}</div>
-        ))}
+      <HeaderContainer
+        header="E-list"
+        headerIcon={faEnvelope}
+        showHorizontalLine={true}
+      >
+        <div className="flex flex-row items-center justify-between">
+          <div className="mb-5 text-xl font-bold">
+            Total ({elistData.length})
+          </div>
+          <CSVSaveButton
+            csvContent={
+              "data:text/csv;charset=utf-8," +
+              [["Email"], ...elistData.map((elistEmail) => [elistEmail.email])]
+                .map((row) => row.join(","))
+                .join("\n")
+            }
+          />
+        </div>
+        <div className="mb-10 grid grid-cols-3 gap-y-5">
+          {columns.map((column, columnIndex) => (
+            <div key={columnIndex}>
+              {column.map((elistEmail, index) => (
+                <div key={index} className="mb-1 underline">
+                  {elistEmail.email}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </HeaderContainer>
     </div>
   );
 };
