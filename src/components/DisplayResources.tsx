f63f874d14a5bd319f1c0ca1c23b3a766bd021ce
tia-chen <qingyan.chen@tufts.edu>
2023-12-08 10:10:19 -0500

    Added edit version UI

--- src/components/DisplayResources.tsx
+++ src/components/DisplayResources.tsx
@@ -1,4 +1,6 @@
 "use client";
+
+import React from "react";
 import ResourceTile from "@components/ResourceTile";
 import { Resource } from "@prisma/client";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -9,29 +11,66 @@ interface IDisplayResources {
   showRole: boolean;
 }
 
+interface ResourceState extends Resource {
+  state: "UNEDITED" | "EDITED" | "DELETED" | "CREATED";
+}
+
 // TODO (johnny) - Create button and use setState to set the isEdit
 const DisplayResources = (props: IDisplayResources) => {
+  const [edit, setEdit] = React.useState(false);
+  const [stateResources, setStateResources] = React.useState<ResourceState[]>(
+    () =>
+      props.resources.map((resource) => ({ state: "UNEDITED", ...resource }))
+  );
+
+  const onDelete = (resource: Resource) => {
+    setStateResources((prev) => {
+      return prev.map((prevResource) => {
+        if (
+          prevResource.id === resource.id &&
+          prevResource.state !== "CREATED"
+        ) {
+          return { ...prevResource, state: "DELETED" };
+        }
+        return prevResource;
+      });
+    });
+  };
+
   return (
     <div className="mt-6">
       <div className="flex justify-between">
         <button className="flex w-44 items-center justify-between rounded-xl border border-dark-teal px-4 py-2.5">
           <p className="text-base font-normal">Add resource</p>
-          <FontAwesomeIcon icon={faPlus} />
+          <FontAwesomeIcon icon={faPlus} className="h-5 w-5" />
         </button>
-        <button className="cursor-pointer text-dark-teal">
-          <span className="mr-2 text-base font-normal">Edit</span>
-          <FontAwesomeIcon icon={faPencil} className="inline" />
-        </button>
+        {edit ? (
+          <button className="rounded-xl bg-dark-teal px-6 py-2 text-white">
+            Save
+          </button>
+        ) : (
+          <button
+            className="cursor-pointer text-dark-teal"
+            onClick={() => setEdit(true)}
+          >
+            <span className="mr-2 text-base font-normal">Edit</span>
+            <FontAwesomeIcon icon={faPencil} className="inline h-5 w-5" />
+          </button>
+        )}
       </div>
       <div className="mt-6 grid items-start gap-6 self-stretch md:grid-cols-2 ">
-        {props.resources.map((eachResource) => (
-          <ResourceTile
-            key={eachResource.id}
-            resource={eachResource}
-            isEdit={false}
-            showRole={props.showRole}
-          />
-        ))}
+        {stateResources
+          .filter((resource) => resource.state !== "DELETED")
+          .map((eachResource) => (
+            <ResourceTile
+              key={eachResource.id}
+              resource={eachResource}
+              isEdit={edit}
+              showRole={props.showRole}
+              onDelete={onDelete}
+              onEdit={(resource) => { return; }}
+            />
+          ))}
       </div>
     </div>
   );
