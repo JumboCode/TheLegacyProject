b2a21edb4ce4a49eca942039c202ec0351d75c46
nickbar01234 <nickbar01234@gmail.com>
2024-02-06 13:56:16 -0500

    Truncate text

--- src/components/TileGrid/UserRequestTile.tsx
+++ src/components/TileGrid/UserRequestTile.tsx
@@ -5,7 +5,7 @@ import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {Prisma} from "@prisma/client";
+import { Prisma } from "@prisma/client";
 import Link from "next/link";
 
 type ChapterWithUser = Prisma.ChapterGetPayload<{
@@ -17,18 +17,14 @@ interface ChapterTileProps {
   topRightButton?: React.ReactNode;
 }
 
-export function UserRequestTile({
-  chapter,
-  topRightButton,
-}: ChapterTileProps) {
-  
+export function UserRequestTile({ chapter, topRightButton }: ChapterTileProps) {
   let yearsActive =
-  (new Date().getTime() - chapter.dateCreated.getTime()) / 1000;
+    (new Date().getTime() - chapter.dateCreated.getTime()) / 1000;
   yearsActive /= 60 * 60 * 24;
   yearsActive = Math.abs(Math.round(yearsActive / 365.25));
-  
+
   const president = React.useMemo(() => {
-    return chapter.students.find(user => user.role === "CHAPTER_LEADER")
+    return chapter.students.find((user) => user.role === "CHAPTER_LEADER");
   }, [chapter]);
 
   return (
@@ -37,22 +33,24 @@ export function UserRequestTile({
         <div className="text-2xl underline">{chapter.chapterName}</div>
         {topRightButton}
       </div>
-      <div className="flex gap-2.5">
-        <div className=" flex w-6/12 flex-col gap-2.5">
+      <div className="flex w-full gap-2.5">
+        <div className="flex w-6/12 flex-col gap-2.5">
           <div>
-            No. of members:&nbsp;<span className="font-bold">{chapter.students.length}</span>
+            No. of members:&nbsp;
+            <span className="font-bold">{chapter.students.length}</span>
           </div>
           <div>
             Years active:&nbsp;<span className="font-bold">{yearsActive}</span>
           </div>
         </div>
         <div className="flex w-6/12 flex-col gap-2.5">
-          <div className="flex">
-            President:&nbsp;<span className="font-bold">{president?.name ?? ""}</span>
+          <div className="w-full truncate">
+            President:&nbsp;
+            <span className="font-bold">{president?.name ?? ""}</span>
           </div>
           <div className="flex items-center gap-1">
             <FontAwesomeIcon icon={faEnvelope} />
-            <span>{president?.email ?? ""}</span>
+            <span className="w-full truncate">{president?.email ?? ""}</span>
           </div>
         </div>
       </div>
