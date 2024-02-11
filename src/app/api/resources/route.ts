7cf8599b1bc7dad176c89fb69a0ca52c67e2e485
nickbar01234 <nickbar01234@gmail.com>
2024-02-11 14:13:14 -0500

    Update response from API

--- src/app/api/resources/route.ts
+++ src/app/api/resources/route.ts
@@ -6,78 +6,72 @@ import {
   batchUpdateRequestSchema,
 } from "./route.schema";
 import { prisma } from "@server/db/client";
-import { invalidFormReponse, unknownErrorResponse } from "../route.schema";
+import { invalidFormReponse } from "../route.schema";
 import { withSessionAndRole } from "@server/decorator";
 
 export const POST = withSessionAndRole(["ADMIN"], async (request) => {
-  try {
-    const resourceRequest = batchCreateRequestSchema.safeParse(
-      await request.req.json()
+  const resourceRequest = batchCreateRequestSchema.safeParse(
+    await request.req.json()
+  );
+  if (!resourceRequest.success) {
+    return NextResponse.json(invalidFormReponse, { status: 400 });
+  } else {
+    const body = resourceRequest.data;
+    const resources = await prisma.$transaction(
+      body.map((resource) => prisma.resource.create({ data: resource }))
     );
-    if (!resourceRequest.success) {
-      return NextResponse.json(invalidFormReponse, { status: 400 });
-    } else {
-      const body = resourceRequest.data;
-      if (body.length > 0) {
-        await prisma.resource.createMany({
-          data: body,
-        });
-      }
-
-      return NextResponse.json(batchResponseSchema.parse({ code: "SUCCESS" }));
-    }
-  } catch {
-    return NextResponse.json(unknownErrorResponse, { status: 500 });
+    return NextResponse.json(
+      batchResponseSchema.parse({ code: "SUCCESS", data: resources })
+    );
   }
 });
 
 export const PUT = withSessionAndRole(["ADMIN"], async (request) => {
-  try {
-    const resourceRequest = batchUpdateRequestSchema.safeParse(
-      await request.req.json()
+  const resourceRequest = batchUpdateRequestSchema.safeParse(
+    await request.req.json()
+  );
+  if (!resourceRequest.success) {
+    return NextResponse.json(invalidFormReponse, { status: 400 });
+  } else {
+    const promises = resourceRequest.data.map((resource) =>
+      prisma.resource.update({
+        where: {
+          id: resource.id,
+        },
+        data: {
+          access: resource.access,
+          title: resource.title,
+          link: resource.link,
+        },
+      })
     );
-    if (!resourceRequest.success) {
-      return NextResponse.json(invalidFormReponse, { status: 400 });
-    } else {
-      const promises = resourceRequest.data.map((resource) =>
-        prisma.resource.update({
-          where: {
-            id: resource.id,
-          },
-          data: {
-            access: resource.access,
-            title: resource.title,
-            link: resource.link,
-          },
-        })
-      );
-      await Promise.allSettled(promises);
+    await Promise.allSettled(promises);
 
-      return NextResponse.json(batchResponseSchema.parse({ code: "SUCCESS" }));
-    }
-  } catch {
-    return NextResponse.json(unknownErrorResponse, { status: 500 });
+    return NextResponse.json(
+      batchResponseSchema.parse({ code: "SUCCESS", data: resourceRequest })
+    );
   }
 });
 
 export const DELETE = withSessionAndRole(["ADMIN"], async (request) => {
-  try {
-    const resourceRequest = batchDeleteRequestSchema.safeParse(
-      await request.req.json()
-    );
-    if (!resourceRequest.success) {
-      return NextResponse.json(invalidFormReponse, { status: 400 });
-    } else {
-      if (resourceRequest.data.length > 0) {
-        await prisma.resource.deleteMany({
+  const resourceRequest = batchDeleteRequestSchema.safeParse(
+    await request.req.json()
+  );
+  if (!resourceRequest.success) {
+    return NextResponse.json(invalidFormReponse, { status: 400 });
+  } else {
+    const body = resourceRequest.data;
+    const resources = await prisma.$transaction(
+      body.map((id) =>
+        prisma.resource.delete({
           where: {
-            id: { in: resourceRequest.data },
+            id: id,
           },
-        });
-      }
-      return NextResponse.json(batchResponseSchema.parse({ code: "SUCCESS" }));
-    }
-  } catch (error) {
-    return NextResponse.json(unknownErrorResponse, { status: 500 });
+        })
+      )
+    );
+    return NextResponse.json(
+      batchResponseSchema.parse({ code: "SUCCESS", data: resources })
+    );
   }
 });
