08ebabf7dbdb1504ec46f1a737e2bb0ac5d035bb
nickbar01234 <nickbar01234@gmail.com>
2024-02-15 01:06:07 -0500

    Fix folder typo

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,6 +1,19 @@
 import { RoleToUrlSegment } from "@constants";
 import { Session } from "next-auth";
+import { Resource } from "@prisma/client";
 
 export const formatUserHomeRoute = (user: NonNullable<Session["user"]>) => {
   return `/private/${user.id}/${RoleToUrlSegment[user.role]}/home`;
+};
+
+/**
+ * Compare resource by alphabetical order.
+ */
+export const compareResource = (x: Resource, y: Resource) => {
+  if (x.title > y.title) {
+    return 1;
+  } else if (x.title < y.title) {
+    return -1;
+  }
+  return 0;
 };
