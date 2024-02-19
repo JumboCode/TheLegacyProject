87fbce779a36504851d4d1837b28ae78d09b639f
nickbar01234 <nickbar01234@gmail.com>
2024-02-18 23:43:07 -0500

    Add input feedback

---
+++ src/components/skeleton/Spinner.tsx
@@ -0,0 +1,11 @@
+const Spinner = () => {
+  // @TODO - Talk to Fa about spinner style
+  return (
+    <svg
+      className="h-20 w-20 animate-spin rounded-full border-8 border-gray-300 border-t-dark-teal"
+      viewBox="0 0 24 24"
+    />
+  );
+};
+
+export default Spinner;
--- src/context/UserProvider.tsx
+++ src/context/UserProvider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Spinner } from "@components/skeleton";
 import { Session } from "next-auth";
 import { useSession } from "next-auth/react";
 import { redirect } from "next/navigation";
@@ -27,13 +28,9 @@ const UserProvider = ({ children }: IUserProvider) => {
   });
 
   if (status === "loading" || data?.user == undefined) {
-    // @TODO - Talk to Fa about spinner style
     return (
       <div className="flex h-full w-full flex-col items-center justify-center">
-        <svg
-          className="h-20 w-20 animate-spin rounded-full border-8 border-gray-300 border-t-dark-teal"
-          viewBox="0 0 24 24"
-        />
+        <Spinner />
       </div>
     );
   } else {
