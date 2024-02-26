e4495bf60153c022b3323487a62e4e077d60d89f
Johnny Tan <jonnytan0818@gmail.com>
2024-02-26 11:33:39 -0500

    implemented navbar styling changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 
 import Link from "next/link";
 import { useAuth } from "@hooks";
+import logoicon from "../../public/landing/newlegacy-logo.svg";
+import Image from "next/image";
 
 const Navbar = () => {
   const [dropdownVisible, setDropdownVisible] = useState(false);
@@ -14,18 +16,18 @@ const Navbar = () => {
 
   return (
     <nav
-      className="top-0 z-10 flex h-[60px] w-full flex-row items-center 
-                    justify-between border border-dark-tan bg-med-tan"
+      className="\ top-0 z-10 flex h-[60px] w-full flex-row items-center 
+                    justify-between border border-dark-tan bg-med-tan px-24 py-2 pt-3"
     >
-      <div className="pl-[20px] font-serif text-xl font-medium sm:pl-[40px] md:text-2xl">
-        <Link href="/">The Legacy Project</Link>
-      </div>
+      <Link href="/public" onClick={() => setDropdownVisible(false)}>
+        <Image src={logoicon} alt="Legacy Logo" className="w-[200px]" />
+      </Link>
 
       <div className="visible z-10 pr-[20px] sm:pr-[40px]">
         <span onClick={handleMenuClick}>
           {dropdownVisible ? (
             <svg
-              className="h-8 w-8 text-darkest-tan sm:hidden"
+              className="md2:hidden h-8 w-8 text-darkest-tan"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -36,7 +38,7 @@ const Navbar = () => {
             </svg>
           ) : (
             <svg
-              className="visible h-8 w-8 text-darkest-tan sm:hidden"
+              className="md2:hidden visible h-8 w-8 text-darkest-tan"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -53,18 +55,33 @@ const Navbar = () => {
       <div
         className={
           dropdownVisible
-            ? "absolute right-0 top-[60px] z-20 flex flex-col items-center gap-[20px] border-b border-l            border-r border-dark-tan bg-med-tan p-[20px] sm:hidden "
-            : "align-center hidden flex-row gap-[20px] pr-[40px] sm:flex"
+            ? "md2:hidden absolute right-0 top-[60px] z-20 flex flex-col items-center gap-[20px] border-b border-l border-r border-dark-tan bg-med-tan p-[20px] "
+            : "align-center md2:flex hidden flex-row gap-[30px] pr-[40px]"
         }
       >
-        <Link href="/">
-          <div className="m-auto font-serif text-lg font-medium duration-150 hover:-translate-y-0.5">
-            About Us
+        <Link href="/public/team" onClick={() => setDropdownVisible(false)}>
+          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+            Meet TLP
+          </div>
+        </Link>
+
+        <Link href="/public/about" onClick={() => setDropdownVisible(false)}>
+          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+            Our Story
+          </div>
+        </Link>
+
+        <Link
+          href="/public/start-chapter"
+          onClick={() => setDropdownVisible(false)}
+        >
+          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+            Start a Chapter
           </div>
         </Link>
 
         <button onClick={onSignIn}>
-          <div className="m-auto font-serif text-lg font-medium duration-150 hover:-translate-y-0.5">
+          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
             {status === "authenticated" ? "Home" : "Sign In"}
           </div>
         </button>
