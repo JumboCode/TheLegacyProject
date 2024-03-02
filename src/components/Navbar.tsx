83a4be544606fb08b39789a114922387e7306c93
Johnny Tan <jonnytan0818@gmail.com>
2024-03-02 12:15:58 -0500

    changed font size and fixed hamburger menu closing on big screens

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Link from "next/link";
 import { useAuth } from "@hooks";
@@ -12,7 +12,21 @@ const Navbar = () => {
   const handleMenuClick: React.MouseEventHandler = () => {
     setDropdownVisible((visible) => !visible);
   };
+
   const { status, onSignIn } = useAuth();
+  useEffect(() => {
+    const dropdownonResize = () => {
+      if (window.innerWidth > 950) {
+        setDropdownVisible(false);
+      }
+    };
+
+    window.addEventListener("resize", dropdownonResize);
+
+    return () => {
+      window.removeEventListener("resize", dropdownonResize);
+    };
+  }, []);
 
   return (
     <nav
@@ -23,11 +37,11 @@ const Navbar = () => {
         <Image src={logoicon} alt="Legacy Logo" className="w-[200px]" />
       </Link>
 
-      <div className="visible z-10 pr-[20px] sm:pr-[40px]">
+      <div className="visible z-10">
         <span onClick={handleMenuClick}>
           {dropdownVisible ? (
             <svg
-              className="md2:hidden h-8 w-8 text-darkest-tan"
+              className="h-8 w-8 text-darkest-tan md2:hidden"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -38,7 +52,7 @@ const Navbar = () => {
             </svg>
           ) : (
             <svg
-              className="md2:hidden visible h-8 w-8 text-darkest-tan"
+              className="visible h-8 w-8 text-darkest-tan md2:hidden"
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
@@ -55,18 +69,18 @@ const Navbar = () => {
       <div
         className={
           dropdownVisible
-            ? "md2:hidden absolute right-0 top-[60px] z-20 flex flex-col items-center gap-[20px] border-b border-l border-r border-dark-tan bg-med-tan p-[20px] "
-            : "align-center md2:flex hidden flex-row gap-[30px] pr-[40px]"
+            ? "absolute right-0 top-[60px] z-20 flex flex-col items-center gap-[20px] border-b border-l border-r border-dark-tan bg-med-tan p-[20px] md2:hidden "
+            : "align-center hidden flex-row gap-[30px] pr-[40px] md2:flex"
         }
       >
         <Link href="/public/team" onClick={() => setDropdownVisible(false)}>
-          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
             Meet TLP
           </div>
         </Link>
 
         <Link href="/public/about" onClick={() => setDropdownVisible(false)}>
-          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
             Our Story
           </div>
         </Link>
@@ -75,13 +89,13 @@ const Navbar = () => {
           href="/public/start-chapter"
           onClick={() => setDropdownVisible(false)}
         >
-          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
             Start a Chapter
           </div>
         </Link>
 
         <button onClick={onSignIn}>
-          <div className="font-merriweather m-auto font-black text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
             {status === "authenticated" ? "Home" : "Sign In"}
           </div>
         </button>
