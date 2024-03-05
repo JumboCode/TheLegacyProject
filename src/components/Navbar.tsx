9daac60faab700425ba3647e3bddb26988d4c8a6
nickbar01234 <nickbar01234@gmail.com>
2024-03-04 23:40:43 -0500

    Add styling fix

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,10 +29,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <nav
-      className="\ top-0 z-10 flex h-[60px] w-full flex-row items-center 
-                    justify-between border border-dark-tan bg-med-tan px-24 py-2 pt-3"
-    >
+    <nav className="top-0 z-10 flex h-[60px] w-full flex-row items-center justify-between border border-dark-tan bg-med-tan px-24 py-3">
       <Link href="/public" onClick={() => setDropdownVisible(false)}>
         <Image src={logoicon} alt="Legacy Logo" className="w-[200px]" />
       </Link>
@@ -74,13 +71,13 @@ const Navbar = () => {
         }
       >
         <Link href="/public/team" onClick={() => setDropdownVisible(false)}>
-          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5">
             Meet TLP
           </div>
         </Link>
 
         <Link href="/public/about" onClick={() => setDropdownVisible(false)}>
-          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5">
             Our Story
           </div>
         </Link>
@@ -89,13 +86,13 @@ const Navbar = () => {
           href="/public/start-chapter"
           onClick={() => setDropdownVisible(false)}
         >
-          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5">
             Start a Chapter
           </div>
         </Link>
 
         <button onClick={onSignIn}>
-          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5 sm:text-sm md:text-lg">
+          <div className="font-merriweather m-auto font-bold text-dark-teal duration-150 hover:-translate-y-0.5">
             {status === "authenticated" ? "Home" : "Sign In"}
           </div>
         </button>
