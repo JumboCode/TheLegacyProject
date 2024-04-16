06648473f0ea53df9994c3d161b62f1a6ac30c26
nickbar01234 <nickbar01234@gmail.com>
2024-04-16 01:09:56 -0400

    Collapse row

--- src/components/NewChapterForm.tsx
+++ src/components/NewChapterForm.tsx
@@ -9,6 +9,8 @@ import { ErrorMessage } from "@hookform/error-message";
 import { createChapterRequest } from "@api/chapter-request/route.client";
 import { useApiThrottle } from "@hooks";
 import { Spinner } from "./skeleton";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAsterisk } from "@fortawesome/free-solid-svg-icons";
 
 type ValidationSchema = z.infer<typeof ChapterRequest>;
 
@@ -56,167 +58,182 @@ const NewChapterForm = () => {
       <div className="w-full rounded-md bg-dark-teal px-9 py-10 text-lg text-white">
         <form onSubmit={handleSubmit(onSubmit)}>
           <div className="grid grid-cols-2 gap-x-10 gap-y-5">
-            <div className="w-full ">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>First Name</div>
               <input
                 {...register("firstName", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="firstName"
-                  render={({ message }) => (
-                    <p>
-                      {" "}
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="firstName"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="w-full">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>Last Name</div>
               <input
                 {...register("lastName", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="lastName"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="lastName"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="w-full">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>University Email</div>
               <input
                 {...register("universityEmail", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="universityEmail"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="universityEmail"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="w-full">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>Phone Number</div>
               <input
                 {...register("phoneNumber", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="phoneNumber"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="phoneNumber"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="w-full">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>College / University</div>
               <input
                 {...register("university", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="university"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="university"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="w-full">
+            <div className="col-span-2 flex w-full flex-col gap-y-1 md:col-span-1">
               <div>College / University Address</div>
               <input
                 {...register("universityAddress", { onChange: resetForm })}
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="universityAddress"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="university"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
           </div>
           <div className="pt-5">
-            <div>
+            <div className="flex flex-col gap-y-1">
               <div>
                 Do you have any experience in student leadership / club
-                organization / storytelling?{" "}
+                organization / storytelling?
               </div>
               <textarea
                 {...register("leadershipExperience", { onChange: resetForm })}
-                className="h-18 w-full resize-y rounded-md px-2 py-2 align-top text-black"
+                className="h-18 w-full resize-none rounded-md px-2 py-2 align-top text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="leadershipExperience"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="university"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="pt-5">
+            <div className="flex flex-col gap-y-1 pt-5">
               <div>
                 What motivates you to start this initiative in your community?{" "}
               </div>
               <textarea
                 {...register("motivation", { onChange: resetForm })}
-                className="h-18 w-full resize-y rounded-md px-2 py-2 align-top text-black"
+                className="h-18 w-full resize-none rounded-md px-2 py-2 align-top text-black"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="motivation"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="university"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="pt-5">
+            <div className="flex flex-col gap-y-1 pt-5">
               <div>
                 Please list three 1 hour time blocks with your avalibility in
                 the next week{" "}
@@ -226,24 +243,26 @@ const NewChapterForm = () => {
                 className="h-8 w-full rounded-md px-2 py-2 text-black"
                 placeholder="Include the date (mm-dd-yyyy), time (hh:mm am/pm), and your timezone"
               />
-              <div className="text-sm">
-                <ErrorMessage
-                  errors={errors}
-                  name="availabilities"
-                  render={({ message }) => (
-                    <p>
-                      <span className="text-sunset-orange">* </span>
-                      {message}
-                    </p>
-                  )}
-                />
-              </div>
+              <ErrorMessage
+                errors={errors}
+                name="university"
+                render={({ message }) => (
+                  <p className="flex items-center gap-x-1 text-sm">
+                    <FontAwesomeIcon
+                      icon={faAsterisk}
+                      className="text-sunset-orange"
+                      size="xs"
+                    />
+                    <p>{message}</p>
+                  </p>
+                )}
+              />
             </div>
-            <div className="pt-5">
+            <div className="flex flex-col gap-y-1 pt-5">
               <div>What questions do you have for us? </div>
               <textarea
                 {...register("questions", { onChange: resetForm })}
-                className="h-8 w-full resize-y rounded-md px-2 py-1 align-top text-black"
+                className="h-12 w-full resize-none rounded-md px-2 py-1 align-top text-black"
               />
             </div>
           </div>
