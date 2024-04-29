5c6fd77372ed81de6f7290e4944c202e39f957c9
Johnny Tan <115383099+johnny-t06@users.noreply.github.com>
2024-04-29 00:16:12 -0400

    Johnny/won/nick/photo carousel 2 (#178)
    * photo carousel attempt
    
    Co-authored-by: Johnny Tan <johnny-t06@users.noreply.github.com>
    Co-authored-by: Nick Doan <nickbar01234@gmail.com>
    Co-authored-by: sburchfield33 <sburchfield33@users.noreply.github.com>
    
    * attempt 2
    
    * ui fixes
    
    ---------
    
    Co-authored-by: wkim10 <wkim10@tufts.edu>
    Co-authored-by: Johnny Tan <johnny-t06@users.noreply.github.com>
    Co-authored-by: Nick Doan <nickbar01234@gmail.com>
    Co-authored-by: sburchfield33 <sburchfield33@users.noreply.github.com>

--- src/components/PhotoCarousel.tsx
+++ src/components/PhotoCarousel.tsx
@@ -1,118 +1,82 @@
-import React, { useState, useEffect, useRef } from "react";
-import Image from "next/legacy/image";
+"use client";
 
-type PhotoProps = {
-  filePath: string;
-};
+import "src/styles/animation.css";
+import React from "react";
+import NextImage from "next/image";
+import "react-loading-skeleton/dist/skeleton.css";
+import Skeleton from "react-loading-skeleton";
+import "react-multi-carousel/lib/styles.css";
+import Carousel from "react-multi-carousel";
 
 interface PhotoCarouselParams {
   imagePaths: string[];
 }
 
-const PhotoCarousel = ({ imagePaths }: PhotoCarouselParams) => {
-  const [photos, setPhotos] = useState<PhotoProps[]>([]);
-  const [activeIndex, setActiveIndex] = useState<number>(0);
-  const [show, setShow] = useState<number>(1);
-  const widthRef = useRef<HTMLHeadingElement>(null);
+const Image = ({ path }: { path: string }) => {
+  const [loaded, setLoaded] = React.useState(false);
+  const [preloaded, setPreloaded] = React.useState(false);
 
-  const [elemWidth, setElemWidth] = useState<number>(0);
-
-  useEffect(() => {
-    function handleResize() {
-      setElemWidth(widthRef.current ? widthRef.current.offsetWidth : 0);
-      const showVal = widthRef.current
-        ? Math.floor(widthRef.current.offsetWidth / 250)
-        : 1;
-      setShow(showVal || 1);
-    }
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+  React.useEffect(() => {
+    setTimeout(() => {
+      setPreloaded(true);
+    }, 2000);
   }, []);
 
-  const carouselHeight = elemWidth / show;
-  const carouselPad = 20;
+  return (
+    <div className={"aspect-square h-full w-full"}>
+      {(!preloaded || !loaded) && <Skeleton className="h-full w-full" />}
+      <NextImage
+        className={preloaded && loaded ? "object-cover" : "invisible"}
+        src={path}
+        alt="Gallery"
+        layout="fill"
+        data-loaded="false"
+        onLoadingComplete={() => {
+          setLoaded(true);
+        }}
+      />
+    </div>
+  );
+};
 
-  const nextIndex = () => {
-    setActiveIndex((prevIndex) => (prevIndex + 1) % photos.length);
+const PhotoCarousel = ({ imagePaths }: PhotoCarouselParams) => {
+  const photos = React.useMemo(
+    () => imagePaths.map((path) => <Image key={path} path={path} />),
+    [imagePaths]
+  );
+
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 4,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
   };
-
-  const prevIndex = () => {
-    setActiveIndex(
-      (prevIndex) => (prevIndex - 1 + photos.length) % photos.length
-    );
-  };
-
-  useEffect(() => {
-    setPhotos(
-      imagePaths.map((path) => ({
-        filePath: path,
-      }))
-    );
-  }, [imagePaths]);
 
   return (
-    <div className="flex w-full flex-col place-content-center gap-y-8 py-6">
-      <div className="relative flex items-center justify-center">
-        <svg
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-          fill="none"
-          className="hover:cursor-pointer"
-          onClick={() => prevIndex()}
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M41.0933 44.24L28.8799 32L41.0933 19.76L37.3333 16L21.3333 32L37.3333 48L41.0933 44.24Z"
-            fill="#000022"
-          />
-        </svg>
-
-        <div
-          className="relative flex w-full flex-row justify-between overflow-hidden ease-in-out"
-          style={{ height: carouselHeight - carouselPad }}
-          ref={widthRef}
-        >
-          {photos.map((photo, index) => (
-            <div
-              key={photo.filePath}
-              className={
-                "absolute aspect-square h-full select-none object-cover transition-all"
-              }
-              style={{
-                left:
-                  ((index - activeIndex + photos.length) % photos.length) *
-                  (carouselHeight + carouselPad / 2),
-              }}
-            >
-              <Image
-                className="object-cover"
-                layout="fill"
-                src={photo.filePath}
-                alt={"object cover"}
-              />
-            </div>
-          ))}
-        </div>
-        <svg
-          width="64"
-          height="64"
-          viewBox="0 0 64 64"
-          fill="none"
-          onClick={() => nextIndex()}
-          className="hover:cursor-pointer"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M22.9067 19.76L35.1201 32L22.9067 44.24L26.6667 48L42.6667 32L26.6667 16L22.9067 19.76Z"
-            fill="#000022"
-          />
-        </svg>
-      </div>
-    </div>
+    <Carousel
+      responsive={responsive}
+      partialVisbile={false}
+      sliderClass="gap-x-4"
+      swipeable={false}
+      ssr
+    >
+      {photos}
+      {/* @note - We add an empty div because the last image is cut */}
+      <div />
+    </Carousel>
   );
 };
 
